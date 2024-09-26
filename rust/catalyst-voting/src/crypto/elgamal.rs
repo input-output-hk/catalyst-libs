594d11483b48c2e4df4c86cef591e06743ce17c3
Mr-Leshiy <leshiy12345678@gmail.com>
2024-09-26 13:20:00 +0300

    add decryption algorithm, add tests

--- rust/catalyst-voting/src/crypto/elgamal.rs
+++ rust/catalyst-voting/src/crypto/elgamal.rs
@@ -1,11 +1,11 @@
 //! Implementation of the lifted ``ElGamal`` cryptosystem, and combine with `ChaCha`
 //! stream cipher to produce a hybrid encryption scheme.
 
-use std::ops::{Add, Mul};
+use std::ops::Mul;
 
 use rand_core::CryptoRngCore;
 
-use super::group::ristretto255::{GroupElement, Scalar};
+use super::group::{GroupElement, Scalar};
 
 /// ``ElGamal`` secret key.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -19,17 +19,61 @@ pub struct PublicKey(GroupElement);
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Ciphertext(GroupElement, GroupElement);
 
+impl SecretKey {
+    /// Generate a random `SecretKey` value from the random number generator.
+    pub fn random<R: CryptoRngCore>(rng: &mut R) -> Self {
+        Self(Scalar::random(rng))
+    }
+
+    /// Generate a corresponding `PublicKey`.
+    pub fn public_key(&self) -> PublicKey {
+        PublicKey(GroupElement::GENERATOR.mul(&self.0))
+    }
+}
+
 /// Given a `message` represented as a `Scalar`, return a ciphertext using the
 /// lifted ``ElGamal`` mechanism.
-/// Returns a ciphertext of type `Ciphertext` and a used randomness.
-pub fn encrypt<R: CryptoRngCore>(
-    message: &Scalar, public_key: &PublicKey, rng: &mut R,
-) -> (Ciphertext, Scalar) {
-    let r = Scalar::random(rng);
-    let e1 = GroupElement::GENERATOR.mul(&r);
+/// Returns a ciphertext of type `Ciphertext`.
+pub fn encrypt(message: &Scalar, public_key: &PublicKey, randomness: &Scalar) -> Ciphertext {
+    let e1 = GroupElement::GENERATOR.mul(randomness);
+    let e2 = &GroupElement::GENERATOR.mul(message) + &public_key.0.mul(randomness);
+    Ciphertext(e1, e2)
+}
 
-    let a = GroupElement::GENERATOR.mul(message);
-    let b = public_key.0.mul(&r);
-    let e2 = a.add(&b);
-    (Ciphertext(e1, e2), r)
+/// Decrypt ``ElGamal`` `Ciphertext`, returns the original message respresented as a
+/// `GroupElement`.
+pub fn decrypt(cipher: &Ciphertext, secret_key: &SecretKey) -> GroupElement {
+    &(&cipher.0 * &secret_key.0.negate()) + &cipher.1
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::{
+        arbitrary::any,
+        prelude::{Arbitrary, BoxedStrategy, Strategy},
+        property_test,
+    };
+
+    use super::*;
+
+    impl Arbitrary for SecretKey {
+        type Parameters = ();
+        type Strategy = BoxedStrategy<Self>;
+
+        fn arbitrary_with((): Self::Parameters) -> Self::Strategy {
+            any::<Scalar>().prop_map(SecretKey).boxed()
+        }
+    }
+
+    #[property_test]
+    fn elgalam_encryption_decryption_test(
+        secret_key: SecretKey, message: Scalar, randomness: Scalar,
+    ) {
+        let public_key = secret_key.public_key();
+
+        let cipher = encrypt(&message, &public_key, &randomness);
+        let decrypted = decrypt(&cipher, &secret_key);
+
+        assert_eq!(decrypted, GroupElement::GENERATOR.mul(&message));
+    }
 }
--- rust/catalyst-voting/src/crypto/group/mod.rs
+++ rust/catalyst-voting/src/crypto/group/mod.rs
@@ -1,6 +1,9 @@
 //! Group definitions used in voting protocol.
 //! For more information, see: <https://input-output-hk.github.io/catalyst-voices/architecture/08_concepts/voting_transaction/crypto/#a-group-definition>
 
-pub mod ristretto255;
+mod ristretto255;
 // Probably it will be used at someday, but for now it is excluded
 // mod utils;
+
+#[allow(clippy::module_name_repetitions)]
+pub use ristretto255::{GroupElement, Scalar};
