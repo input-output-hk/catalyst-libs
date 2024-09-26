0a5bb99dcdd5462a803f9110da82b83403152880
Mr-Leshiy <leshiy12345678@gmail.com>
2024-09-26 16:03:28 +0300

    remove unused std_ops_gen

--- rust/catalyst-voting/src/crypto/group/mod.rs
+++ rust/catalyst-voting/src/crypto/group/mod.rs
@@ -2,8 +2,6 @@
 //! For more information, see: <https://input-output-hk.github.io/catalyst-voices/architecture/08_concepts/voting_transaction/crypto/#a-group-definition>
 
 mod ristretto255;
-// Probably it will be used at someday, but for now it is excluded
-// mod utils;
 
 #[allow(clippy::module_name_repetitions)]
 pub use ristretto255::{GroupElement, Scalar};
