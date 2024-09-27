88f3a5bfe0fb8a078b7454c1a46a099e982b07ee
Alex Pozhylenkov <leshiy12345678@gmail.com>
2024-09-27 17:30:58 +0300

    feat(rust/catalyst-voting): Added basic ElGamal encryption/decryption implementation (#35)
    * initialize a new crate
    
    * add intentionally failed test
    
    * fix CI
    
    * fix
    
    * fix
    
    * update vscode setting.recommended.json
    
    * add a basic interfaces for the vote part
    
    * add basic elgamal encryption based on the ristretto255 group
    
    * add arithmetic tests for ristretto255
    
    * fix tests
    
    * wip
    
    * add decryption algorithm, add tests
    
    * fix CI
    
    * remove unused std_ops_gen

---
+++ rust/catalyst-voting/src/crypto/group/mod.rs
@@ -0,0 +1,7 @@
+//! Group definitions used in voting protocol.
+//! For more information, see: <https://input-output-hk.github.io/catalyst-voices/architecture/08_concepts/voting_transaction/crypto/#a-group-definition>
+
+mod ristretto255;
+
+#[allow(clippy::module_name_repetitions)]
+pub use ristretto255::{GroupElement, Scalar};
