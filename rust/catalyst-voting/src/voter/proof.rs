76c78c19bdedfdc3ec46f458ed1d22436e64db30
Mr-Leshiy <leshiy12345678@gmail.com>
2024-10-07 12:44:43 +0300

    add error handling for `generate_voter_proof`

--- rust/catalyst-voting/src/voter/proof.rs
+++ rust/catalyst-voting/src/voter/proof.rs
@@ -28,13 +28,32 @@ impl VoterProofCommitment {
     }
 }
 
+/// Generate voter proof error
+#[derive(thiserror::Error, Debug)]
+pub enum GenerateVoterProofError {
+    /// Arguments mismatch
+    #[error("Provided arguments mismatch. Size of the provided `vote`: {0}, `encrypted_vote: {1}` and `randomness`: {2} must be equal with each other.")]
+    ArgumentsMismatch(usize, usize, usize),
+}
+
 /// Generates a voter proof.
 /// More detailed described [here](https://input-output-hk.github.io/catalyst-voices/architecture/08_concepts/voting_transaction/crypto/#voters-proof)
+///
+/// # Errors
+///   - `GenerateVoterProofError`
 #[allow(clippy::module_name_repetitions)]
 pub fn generate_voter_proof<R: CryptoRngCore>(
     vote: &Vote, encrypted_vote: EncryptedVote, randomness: EncryptionRandomness,
     public_key: &PublicKey, commitment: &VoterProofCommitment, rng: &mut R,
-) -> VoterProof {
+) -> Result<VoterProof, GenerateVoterProofError> {
+    if vote.voting_options != encrypted_vote.0.len() || vote.voting_options != randomness.0.len() {
+        return Err(GenerateVoterProofError::ArgumentsMismatch(
+            vote.voting_options,
+            encrypted_vote.0.len(),
+            randomness.0.len(),
+        ));
+    }
+
     let proof = generate_unit_vector_proof(
         &vote.to_unit_vector(),
         encrypted_vote.0,
@@ -43,7 +62,7 @@ pub fn generate_voter_proof<R: CryptoRngCore>(
         &commitment.0,
         rng,
     );
-    VoterProof(proof)
+    Ok(VoterProof(proof))
 }
 
 /// Verifies a voter proof.
