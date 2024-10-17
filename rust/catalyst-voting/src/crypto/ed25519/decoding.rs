bbd21ba236a064df7e149d49e979df135ea59fe8
Mr-Leshiy <leshiy12345678@gmail.com>
2024-10-17 11:06:17 +0300

    add decoding impl

--- rust/catalyst-voting/src/crypto/ed25519/decoding.rs
+++ rust/catalyst-voting/src/crypto/ed25519/decoding.rs
@@ -1,10 +1,31 @@
 //! `Ed25519` objects decoding implementation
 
+use anyhow::anyhow;
 use ed25519_dalek::{
-    Signature as Ed25519Signature, VerifyingKey, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH,
+    Signature as Ed25519Signature, SigningKey, VerifyingKey, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH,
+    SIGNATURE_LENGTH,
 };
 
-use super::{PublicKey, Signature};
+use super::{PrivateKey, PublicKey, Signature};
+
+impl PrivateKey {
+    /// `PrivateKey` bytes size
+    pub const BYTES_SIZE: usize = SECRET_KEY_LENGTH;
+
+    /// Convert this `PrivateKey` to its underlying sequence of bytes.
+    #[must_use]
+    pub fn to_bytes(&self) -> [u8; Self::BYTES_SIZE] {
+        self.0.to_bytes()
+    }
+
+    /// Attempt to construct a `PrivateKey` from a byte representation.
+    ///
+    /// # Errors
+    ///   - Cannot decode public key.
+    pub fn from_bytes(bytes: &[u8; Self::BYTES_SIZE]) -> Self {
+        Self(SigningKey::from_bytes(bytes))
+    }
+}
 
 impl PublicKey {
     /// `PublicKey` bytes size
@@ -21,7 +42,9 @@ impl PublicKey {
     /// # Errors
     ///   - Cannot decode public key.
     pub fn from_bytes(bytes: &[u8; Self::BYTES_SIZE]) -> anyhow::Result<Self> {
-        Ok(Self(VerifyingKey::from_bytes(bytes)?))
+        Ok(Self(
+            VerifyingKey::from_bytes(bytes).map_err(|_| anyhow!("Cannot decode public key."))?,
+        ))
     }
 }
 
