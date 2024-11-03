e3a1fbe9576d8b92e247f0d673cf24a8730abc5f
cong-or <conchubhar.gannon@gmail.com>
2024-11-03 20:22:15 +0000

    refactor(add block types and cddl tests): verifcation and refactor

--- rust/immutable-ledger/src/lib.rs
+++ rust/immutable-ledger/src/lib.rs
@@ -4,8 +4,8 @@
 //!
 //! Spec: `<https://input-output-hk.github.io/catalyst-voices/architecture/08_concepts/immutable_ledger/ledger>`
 
-/// Block validation logic
-pub mod validate;
-
-/// Block encoding decoding
+/// Block encoding decoding and validation
 pub mod serialize;
+
+/// ac
+pub mod ser;
