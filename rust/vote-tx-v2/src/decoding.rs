3fc98c850d812e7094c63a20a6d7cedd2e039594
Mr-Leshiy <leshiy12345678@gmail.com>
2024-11-11 15:45:22 +0200

    add TxBody CBOR decoding/encoding impl

--- rust/vote-tx-v2/src/decoding.rs
+++ rust/vote-tx-v2/src/decoding.rs
@@ -3,13 +3,22 @@
 use anyhow::anyhow;
 use minicbor::{data::IanaTag, Decode, Decoder, Encode, Encoder};
 
-use crate::{Choice, Proof, PropId, Vote};
+use crate::{Choice, Proof, PropId, TxBody, Uuid, Vote, VoterData};
 
-impl Vote {
-    /// Encodes `Vote` to CBOR encoded bytes.
+/// UUID CBOR tag <https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml/>.
+const CBOR_UUID_TAG: u64 = 37;
+
+/// `Vote` array struct length
+const VOTE_LEN: u64 = 3;
+
+/// `TxBody` array struct length
+const TX_BODY_LEN: u64 = 3;
+
+impl TxBody {
+    /// Encodes `TxBody` to CBOR encoded bytes.
     ///
     /// # Errors
-    ///  - Cannot encode `Vote`
+    ///  - Cannot encode `TxBody`
     pub fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
         let mut bytes = Vec::new();
         let mut e = Encoder::new(&mut bytes);
@@ -18,21 +27,106 @@ impl Vote {
         Ok(bytes)
     }
 
-    /// Decodes `Vote` from the CBOR encoded bytes.
+    /// Decodes `TxBody` from the CBOR encoded bytes.
     ///
     /// # Errors
-    ///  - Cannot decode `Vote`
+    ///  - Cannot decode `TxBody`
     pub fn from_bytes(bytes: &[u8]) -> anyhow::Result<Self> {
         let mut decoder = Decoder::new(bytes);
-        let res = Vote::decode(&mut decoder, &mut ())
+        let res = Self::decode(&mut decoder, &mut ())
             .map_err(|e| anyhow!("Cannot decode `{}`, {e}.", std::any::type_name::<Self>()))?;
         Ok(res)
     }
 }
 
+impl Decode<'_, ()> for TxBody {
+    fn decode(d: &mut Decoder<'_>, (): &mut ()) -> Result<Self, minicbor::decode::Error> {
+        let Some(TX_BODY_LEN) = d.array()? else {
+            return Err(minicbor::decode::Error::message(format!(
+                "must be a defined sized array with {TX_BODY_LEN} entries"
+            )));
+        };
+
+        let vote_type = Uuid::decode(d, &mut ())?;
+        let votes = Vec::<Vote>::decode(d, &mut ())?;
+        let voter_data = VoterData::decode(d, &mut ())?;
+        Ok(TxBody {
+            vote_type,
+            votes,
+            voter_data,
+        })
+    }
+}
+
+impl Encode<()> for TxBody {
+    fn encode<W: minicbor::encode::Write>(
+        &self, e: &mut Encoder<W>, (): &mut (),
+    ) -> Result<(), minicbor::encode::Error<W::Error>> {
+        e.array(TX_BODY_LEN)?;
+        self.vote_type.encode(e, &mut ())?;
+        self.votes.encode(e, &mut ())?;
+        self.voter_data.encode(e, &mut ())?;
+        Ok(())
+    }
+}
+
+impl Decode<'_, ()> for VoterData {
+    fn decode(d: &mut Decoder<'_>, (): &mut ()) -> Result<Self, minicbor::decode::Error> {
+        let tag = d.tag()?;
+        let expected_tag = minicbor::data::IanaTag::Cbor.tag();
+        if expected_tag != tag {
+            return Err(minicbor::decode::Error::message(format!(
+                "tag value must be: {}, provided: {}",
+                expected_tag.as_u64(),
+                tag.as_u64(),
+            )));
+        }
+        let choice = d.bytes()?.to_vec();
+        Ok(Self(choice))
+    }
+}
+
+impl Encode<()> for VoterData {
+    fn encode<W: minicbor::encode::Write>(
+        &self, e: &mut minicbor::Encoder<W>, (): &mut (),
+    ) -> Result<(), minicbor::encode::Error<W::Error>> {
+        e.tag(IanaTag::Cbor.tag())?;
+        e.bytes(&self.0)?;
+        Ok(())
+    }
+}
+
+impl Decode<'_, ()> for Uuid {
+    fn decode(d: &mut Decoder<'_>, (): &mut ()) -> Result<Self, minicbor::decode::Error> {
+        let tag = d.tag()?;
+        if CBOR_UUID_TAG != tag.as_u64() {
+            return Err(minicbor::decode::Error::message(format!(
+                "tag value must be: {CBOR_UUID_TAG}, provided: {}",
+                tag.as_u64(),
+            )));
+        }
+        let choice = d.bytes()?.to_vec();
+        Ok(Self(choice))
+    }
+}
+
+impl Encode<()> for Uuid {
+    fn encode<W: minicbor::encode::Write>(
+        &self, e: &mut minicbor::Encoder<W>, (): &mut (),
+    ) -> Result<(), minicbor::encode::Error<W::Error>> {
+        e.tag(IanaTag::Cbor.tag())?;
+        e.bytes(&self.0)?;
+        Ok(())
+    }
+}
+
 impl Decode<'_, ()> for Vote {
     fn decode(d: &mut Decoder<'_>, (): &mut ()) -> Result<Self, minicbor::decode::Error> {
-        d.array()?;
+        let Some(VOTE_LEN) = d.array()? else {
+            return Err(minicbor::decode::Error::message(format!(
+                "must be a defined sized array with {VOTE_LEN} entries"
+            )));
+        };
 
         let choices = Vec::<Choice>::decode(d, &mut ())?;
         if choices.is_empty() {
@@ -55,8 +149,7 @@ impl Encode<()> for Vote {
     fn encode<W: minicbor::encode::Write>(
         &self, e: &mut minicbor::Encoder<W>, (): &mut (),
     ) -> Result<(), minicbor::encode::Error<W::Error>> {
-        e.array(3)?;
-
+        e.array(VOTE_LEN)?;
         self.choices.encode(e, &mut ())?;
         self.proof.encode(e, &mut ())?;
         self.prop_id.encode(e, &mut ())?;
@@ -76,7 +169,7 @@ impl Decode<'_, ()> for Choice {
             )));
         }
         let choice = d.bytes()?.to_vec();
-        Ok(Choice(choice))
+        Ok(Self(choice))
     }
 }
 
@@ -102,7 +195,7 @@ impl Decode<'_, ()> for Proof {
             )));
         }
         let choice = d.bytes()?.to_vec();
-        Ok(Proof(choice))
+        Ok(Self(choice))
     }
 }
 
@@ -128,7 +221,7 @@ impl Decode<'_, ()> for PropId {
             )));
         }
         let choice = d.bytes()?.to_vec();
-        Ok(PropId(choice))
+        Ok(Self(choice))
     }
 }
 
@@ -147,12 +240,12 @@ mod tests {
     use proptest::prelude::ProptestConfig;
     use test_strategy::proptest;
 
-    use super::Vote;
+    use super::TxBody;
 
     #[proptest(ProptestConfig::with_cases(0))]
-    fn vote_from_bytes_to_bytes_test(vote: Vote) {
-        let bytes = vote.to_bytes().unwrap();
-        let decoded = Vote::from_bytes(&bytes).unwrap();
-        assert_eq!(vote, decoded);
+    fn vote_from_bytes_to_bytes_test(tx_body: TxBody) {
+        let bytes = tx_body.to_bytes().unwrap();
+        let decoded = TxBody::from_bytes(&bytes).unwrap();
+        assert_eq!(tx_body, decoded);
     }
 }
