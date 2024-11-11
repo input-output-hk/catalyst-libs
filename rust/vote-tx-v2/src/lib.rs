1f9855d535e5d88fa41353d94858617b6ffb3abb
Mr-Leshiy <leshiy12345678@gmail.com>
2024-11-11 15:30:11 +0200

    add new TxBody struct, cleanup gen_vote_tx.cddl

--- rust/vote-tx-v2/src/lib.rs
+++ rust/vote-tx-v2/src/lib.rs
@@ -3,6 +3,17 @@
 
 mod decoding;
 
+/// A tx body struct.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TxBody {
+    /// `vote-type` field
+    vote_type: Uuid,
+    /// `votes` field
+    votes: Vec<Vote>,
+    /// `voter-data` field
+    voter_data: VoterData,
+}
+
 /// A vote struct.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vote {
@@ -13,6 +24,14 @@ pub struct Vote {
     /// `prop-id` field
     prop_id: PropId,
 }
+
+/// A UUID struct.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Uuid(Vec<u8>);
+
+/// A voter's data struct.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct VoterData(Vec<u8>);
 
 /// A choice struct.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -29,11 +48,29 @@ pub struct PropId(Vec<u8>);
 #[allow(missing_docs, clippy::missing_docs_in_private_items)]
 mod arbitrary_impl {
     use proptest::{
-        prelude::{any_with, Arbitrary, BoxedStrategy, Strategy},
+        prelude::{any, any_with, Arbitrary, BoxedStrategy, Strategy},
         sample::size_range,
     };
 
-    use super::{Choice, Proof, PropId, Vote};
+    use super::{Choice, Proof, PropId, Uuid, Vote, VoterData};
+    use crate::TxBody;
+
+    impl Arbitrary for TxBody {
+        type Parameters = ();
+        type Strategy = BoxedStrategy<Self>;
+
+        fn arbitrary_with((): Self::Parameters) -> Self::Strategy {
+            any::<(Vec<u8>, Vec<Vote>, Vec<u8>)>()
+                .prop_map(|(vote_type, votes, voters_data)| {
+                    Self {
+                        vote_type: Uuid(vote_type),
+                        votes,
+                        voter_data: VoterData(voters_data),
+                    }
+                })
+                .boxed()
+        }
+    }
 
     impl Arbitrary for Vote {
         type Parameters = ();
