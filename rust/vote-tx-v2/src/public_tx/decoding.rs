d47b09ee19a972e5c95c23826c5e430e56e8be20
Mr-Leshiy <leshiy12345678@gmail.com>
2024-11-15 16:32:51 +0200

    add initial PublicTx impl

--- rust/vote-tx-v2/src/public_tx/decoding.rs
+++ rust/vote-tx-v2/src/public_tx/decoding.rs
@@ -1,2 +1,21 @@
 //! CBOR encoding and decoding implementation.
 //! <https://input-output-hk.github.io/catalyst-libs/architecture/08_concepts/catalyst_voting/cddl/vote_tx_v2_public.cddl>
+
+use minicbor::{Decode, Encode};
+
+use super::{GeneralizedTx, PublicTx};
+
+impl Decode<'_, ()> for PublicTx {
+    fn decode(d: &mut minicbor::Decoder<'_>, (): &mut ()) -> Result<Self, minicbor::decode::Error> {
+        let gen_tx = GeneralizedTx::decode(d, &mut ())?;
+        Ok(PublicTx(gen_tx))
+    }
+}
+
+impl Encode<()> for PublicTx {
+    fn encode<W: minicbor::encode::Write>(
+        &self, e: &mut minicbor::Encoder<W>, ctx: &mut (),
+    ) -> Result<(), minicbor::encode::Error<W::Error>> {
+        self.0.encode(e, ctx)
+    }
+}
--- rust/vote-tx-v2/src/public_tx/mod.rs
+++ rust/vote-tx-v2/src/public_tx/mod.rs
@@ -1,4 +1,31 @@
 //! A Catalyst public vote transaction v2 object, structured following this
 //! [spec](https://input-output-hk.github.io/catalyst-libs/architecture/08_concepts/catalyst_voting/v2/#public-vote)
 
+use std::ops::{Deref, DerefMut};
+
+use crate::gen_tx::{GeneralizedTx, Uuid};
+
 mod decoding;
+
+/// A public vote tx struct.
+pub struct PublicTx(GeneralizedTx);
+
+/// A public voting choice struct.
+pub struct Choice(pub u64);
+
+/// A public voting proposal struct.
+pub struct Proposal(pub Uuid);
+
+impl Deref for PublicTx {
+    type Target = GeneralizedTx;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for PublicTx {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
