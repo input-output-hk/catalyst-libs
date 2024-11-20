8e18b4ae669948cea226d082db929368f903cf58
Mr-Leshiy <leshiy12345678@gmail.com>
2024-11-20 10:42:05 +0200

    cleanup tests/identifiers.rs

--- rust/cbork-cddl-parser/tests/identifiers.rs
+++ rust/cbork-cddl-parser/tests/identifiers.rs
@@ -1,32 +1,42 @@
 // cspell: words aname groupsocket typesocket groupsocket
 
 mod common;
-use common::{identifiers::*, CDDLTestParser, Rule};
-use pest::Parser;
+use common::{identifiers::*, Rule};
 
-#[test]
 /// Check if the name components pass properly.
-fn check_name_characters() {
-    for x in ('\u{0}'..='\u{ff}').map(char::from) {
-        let test = format!("{x}");
-        let parse_start = CDDLTestParser::parse(Rule::NAME_START, &test);
-        let parse_end = CDDLTestParser::parse(Rule::NAME_END, &test);
+#[test]
+fn check_name_start_characters() {
+    let passes = ('\u{0}'..='\u{ff}')
+        .filter(|x| x.is_ascii_alphabetic() || matches!(x, '@' | '_' | '$'))
+        .map(String::from)
+        .collect::<Vec<_>>();
+    let fails = ('\u{0}'..='\u{ff}')
+        .filter(|x| !x.is_ascii_alphabetic() && !matches!(x, '@' | '_' | '$'))
+        .map(String::from)
+        .collect::<Vec<_>>();
 
-        if x.is_ascii_alphabetic() || matches!(x, '@' | '_' | '$') {
-            assert!(parse_start.is_ok());
-            assert!(parse_end.is_ok());
-        } else if x.is_ascii_digit() {
-            assert!(parse_start.is_err());
-            assert!(parse_end.is_ok());
-        } else {
-            assert!(parse_start.is_err());
-            assert!(parse_end.is_err());
-        }
-    }
+    common::check_tests_rule(Rule::NAME_START, &passes, &fails);
 }
 
+/// Check if the name components pass properly.
 #[test]
+fn check_name_end_characters() {
+    let passes = ('\u{0}'..='\u{ff}')
+        .filter(|x| x.is_ascii_alphabetic() || x.is_ascii_digit() || matches!(x, '@' | '_' | '$'))
+        .map(String::from)
+        .collect::<Vec<_>>();
+    let fails = ('\u{0}'..='\u{ff}')
+        .filter(|x| {
+            !x.is_ascii_alphabetic() && !x.is_ascii_digit() && !matches!(x, '@' | '_' | '$')
+        })
+        .map(String::from)
+        .collect::<Vec<_>>();
+
+    common::check_tests_rule(Rule::NAME_END, &passes, &fails);
+}
+
 /// Test if the `id` rule passes properly.
+#[test]
 fn check_id() {
     common::check_tests_rule(Rule::id_TEST, ID_PASSES, ID_FAILS);
 }
