bb43028314ae1a7a77889dfb8d871aea6dbb8e1a
bkioshn <bkioshn@gmail.com>
2024-11-26 20:12:52 +0700

    fix: add Bytes conversion for Pubkey
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/rbac-registration/src/cardano/cip509/rbac/pub_key.rs
+++ rust/rbac-registration/src/cardano/cip509/rbac/pub_key.rs
@@ -28,6 +28,24 @@ impl From<[u8; 32]> for Ed25519PublicKey {
     }
 }
 
+impl TryFrom<Bytes> for Ed25519PublicKey {
+    type Error = &'static str;
+
+    fn try_from(bytes: Bytes) -> Result<Self, Self::Error> {
+        let byte_vec: Vec<u8> = bytes.into();
+
+        if byte_vec.len() != 32 {
+            return Err("Invalid length for Ed25519 public key: expected 32 bytes.");
+        }
+
+        let byte_array: [u8; 32] = byte_vec
+            .try_into()
+            .map_err(|_| "Failed to convert Vec<u8> to [u8; 32]")?;
+
+        Ok(Ed25519PublicKey::from(byte_array))
+    }
+}
+
 impl From<Ed25519PublicKey> for Bytes {
     fn from(val: Ed25519PublicKey) -> Self {
         let vec: Vec<u8> = val.0.to_vec();
