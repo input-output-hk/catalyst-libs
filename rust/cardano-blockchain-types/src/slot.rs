3a74de4ad89c4028be92519e27535ec559bfb577
bkioshn <bkioshn@gmail.com>
2024-12-26 10:13:21 +0700

    fix(cardano-blockchain-types): add more trait to slot
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/cardano-blockchain-types/src/slot.rs
+++ rust/cardano-blockchain-types/src/slot.rs
@@ -1,7 +1,11 @@
 //! Block Slot
+
+use std::{cmp::Ordering, ops::{MulAssign, Sub}};
+
 use crate::conversion::from_saturating;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Default)]
+
 /// Slot on the blockchain, typically one slot equals one second.  However chain
 /// parameters can alter how long a slot is.
 pub struct Slot(u64);
@@ -31,5 +35,25 @@ impl From<u64> for Slot {
 impl From<Slot> for u64 {
     fn from(val: Slot) -> Self {
         val.0
+    }
+}
+
+impl MulAssign<u64> for Slot {
+    fn mul_assign(&mut self, rhs: u64) {
+        self.0 *= rhs;
+    }
+}
+
+impl PartialOrd for Slot {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Sub for Slot {
+    type Output = Slot;
+
+    fn sub(self, rhs: Slot) -> Self::Output {
+        Slot(self.0.saturating_sub(rhs.0))
     }
 }
