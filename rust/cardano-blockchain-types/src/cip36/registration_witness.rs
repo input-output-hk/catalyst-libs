5fb2094ceff8149113535b2c7e399c8ac4fa5d5f
bkioshn <bkioshn@gmail.com>
2025-01-02 18:25:53 +0700

    fix(cardano-blockchain-types): registration witness
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/cardano-blockchain-types/src/cip36/registration_witness.rs
+++ rust/cardano-blockchain-types/src/cip36/registration_witness.rs
@@ -25,24 +25,35 @@ impl Decode<'_, ()> for Cip36RegistrationWitness {
     fn decode(d: &mut Decoder, ctx: &mut ()) -> Result<Self, decode::Error> {
         let map_len = decode_map_len(d, "CIP36 Registration Witness")?;
 
+        // Record of errors found during decoding
+        let mut err_report = Vec::new();
+
         // Expected only 1 key in the map.
         if map_len != 1 {
-            return Err(decode::Error::message(format!(
+            err_report.push(format!(
                 "Invalid CIP36 Registration Witness map length, expected 1, got {map_len}"
-            )));
+            ));
         }
 
         let key: u16 = decode_helper(d, "key in CIP36 Registration Witness", ctx)?;
 
         // The key needs to be 1.
         if key != 1 {
-            return Err(decode::Error::message(format!(
+            err_report.push(format!(
                 "Invalid CIP36 Registration Witness key, expected key 1, got {key}"
-            )));
+            ));
         }
 
         let sig_bytes = decode_bytes(d, "CIP36 Registration Witness signature")?;
-        let signature = ed25519_dalek::Signature::from_slice(&sig_bytes).ok();
+        let signature = ed25519_dalek::Signature::from_slice(&sig_bytes)
+            .map_err(|_| {
+                err_report.push("Invalid CIP36 Registration Witness signature".to_string());
+            })
+            .ok();
+
+        if !err_report.is_empty() {
+            return Err(decode::Error::message(format!("{err_report:?}")));
+        }
 
         Ok(Cip36RegistrationWitness { signature })
     }
