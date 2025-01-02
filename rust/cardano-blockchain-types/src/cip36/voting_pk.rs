b646bb751f46f5317b84f8d3481f6786df20108e
bkioshn <bkioshn@gmail.com>
2025-01-02 18:26:07 +0700

    fix(cardano-blockchain-types): voting pk
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/cardano-blockchain-types/src/cip36/voting_pk.rs
+++ rust/cardano-blockchain-types/src/cip36/voting_pk.rs
@@ -3,11 +3,10 @@
 use ed25519_dalek::VerifyingKey;
 
 /// Voting public key containing the public key and weight.
-#[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub struct VotingPubKey {
     /// Voting public key.
-    pub voting_pk: VerifyingKey,
+    pub voting_pk: Option<VerifyingKey>,
     /// Voting key associated weight.
     pub weight: u32,
 }
