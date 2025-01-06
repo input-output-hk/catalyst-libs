bfa2fc3eb0aabd6dbce808741c66bd0eb4c8bec6
bkioshn <35752733+bkioshn@users.noreply.github.com>
2025-01-06 14:30:16 +0700

    feat(rust/catalyst-types): Add duplicate data error report (#141)
    * feat(catalyst-types): add duplicate data error report
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(catalyst-types): format
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(catalyst-types): add duplicate field description and change name
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * Update rust/catalyst-types/src/problem_report.rs
    
    ---------
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    Co-authored-by: Steven Johnson <stevenj@users.noreply.github.com>

--- rust/catalyst-types/src/problem_report.rs
+++ rust/catalyst-types/src/problem_report.rs
@@ -48,6 +48,13 @@ enum Kind {
         /// Explanation of the failed or problematic validation
         explanation: String,
     },
+    /// Duplicate field was detected.
+    DuplicateField {
+        /// The duplicated field.
+        field: String,
+        /// Additional information about the duplicate field.
+        description: String,
+    },
     /// An uncategorized problem was encountered. Use only for rare problems, otherwise
     /// make a new problem kind.
     Other {
@@ -332,6 +339,40 @@ impl ProblemReport {
         self.add_entry(
             Kind::FunctionalValidation {
                 explanation: explanation.to_owned(),
+            },
+            context,
+        );
+    }
+
+    /// Reports that duplicate field was detected in the problem report.
+    ///
+    /// This method adds an entry to the problem report indicating that duplicate field
+    /// is found, along with the description of the duplicate field and any additional
+    /// context.
+    ///
+    /// # Arguments
+    ///
+    /// * `field`: A string slice representing the value of the duplicate field.
+    /// * `description`: An additional information about the duplicate field.
+    /// * `context`: A string slice providing additional context or information about
+    ///   where and why this duplicate field was detected.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use catalyst_types::problem_report::ProblemReport;
+    /// let report = ProblemReport::new("RBAC Registration Decoding");
+    /// report.duplicate_field(
+    ///     "key 0",
+    ///     "key is already defined, redundant key found in item 6 in RBAC map",
+    ///     "RBAC purpose",
+    /// );
+    /// ```
+    pub fn duplicate_field(&self, field: &str, description: &str, context: &str) {
+        self.add_entry(
+            Kind::DuplicateField {
+                field: field.to_owned(),
+                description: description.to_owned(),
             },
             context,
         );
