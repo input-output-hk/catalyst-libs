132acdb6456d9d9bc3d9f9df16de33455183df92
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-01-06 14:26:16 +0700

    test: integration

---
+++ rust/catalyst-types/tests/mod.rs
@@ -0,0 +1,19 @@
+//! Integration test for `catalyst-type`
+//! 
+//! This is mainly to demonstrate sample usage of the types.
+
+use catalyst_types;
+
+#[allow(dead_code)]
+#[test]
+fn test_type_usage() {
+    type A = catalyst_types::hashes::Blake2b128Hash;
+    type B = catalyst_types::hashes::Blake2b224Hash;
+    type C = catalyst_types::hashes::Blake2b256Hash;
+
+    type D = catalyst_types::uuid::UuidV4;
+    type E = catalyst_types::uuid::UuidV7;
+
+    let bytes: [u8; 32] = [0; 32];
+    let _ = catalyst_types::hashes::Blake2bHash::from(bytes);
+}
