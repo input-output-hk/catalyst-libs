45e97860c693c45f7ca5ad4c6b8bbc5bac167870
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-01-11 01:21:02 +0100

    Test decremented_index

--- rust/rbac-registration/src/utils/general.rs
+++ rust/rbac-registration/src/utils/general.rs
@@ -1,14 +1,23 @@
 //! General utility functions
 
+use anyhow::Context;
+
 /// Getting the index by decrementing by 1.
 /// e.g. 1 should refers to index 0
 pub(crate) fn decremented_index(int: i16) -> anyhow::Result<usize> {
-    match usize::try_from(int) {
-        Ok(value) => Ok(value - 1),
-        Err(e) => {
-            Err(anyhow::Error::msg(format!(
-                "Failed to convert to usize: {e}"
-            )))
-        },
+    int.checked_sub(1)
+        .and_then(|v| usize::try_from(v).ok())
+        .context("Failed to convert '{int}' to usize: {e:?}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decremented_index() {
+        assert_eq!(0, decremented_index(1).unwrap());
+        decremented_index(0).unwrap_err();
+        decremented_index(-1).unwrap_err();
     }
 }
