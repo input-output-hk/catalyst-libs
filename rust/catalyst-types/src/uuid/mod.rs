f47590e3be46f63c6c406fe68942057b1dff3f49
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-01-14 21:17:50 -0600

    fix(rust/catalyst-types): fix uuid cbor

--- rust/catalyst-types/src/uuid/mod.rs
+++ rust/catalyst-types/src/uuid/mod.rs
@@ -1,7 +1,7 @@
 //! `UUID` types.
 
 use displaydoc::Display;
-use minicbor::Decoder;
+use minicbor::{data::Tag, Decoder};
 use thiserror::Error;
 use uuid::Uuid;
 
@@ -13,6 +13,9 @@ pub use uuid_v7::UuidV7 as V7;
 
 /// Invalid Doc Type UUID
 pub const INVALID_UUID: uuid::Uuid = uuid::Uuid::from_bytes([0x00; 16]);
+
+/// UUID CBOR tag <https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml/>.
+pub(crate) const UUID_CBOR_TAG: u64 = 37;
 
 /// Errors that can occur when decoding CBOR-encoded UUIDs.
 #[derive(Display, Debug, Error)]
@@ -34,9 +37,17 @@ pub enum CborUuidError {
 pub(crate) fn decode_cbor_uuid(
     d: &mut Decoder<'_>, (): &mut (),
 ) -> Result<uuid::Uuid, minicbor::decode::Error> {
-    let decoded = d.bytes()?.try_into().map_err(|e| {
-        minicbor::decode::Error::message(format!("Expected UUID to have 16 bytes: {e}"))
-    })?;
+    let tag = d.tag()?;
+    if UUID_CBOR_TAG != tag.as_u64() {
+        return Err(minicbor::decode::Error::message(format!(
+            "tag value must be: {UUID_CBOR_TAG}, provided: {}",
+            tag.as_u64(),
+        )));
+    }
+    let decoded = d
+        .bytes()?
+        .try_into()
+        .map_err(|_| minicbor::decode::Error::message("Expected UUID to have 16 bytes"))?;
     let uuid = uuid::Uuid::from_bytes(decoded);
     Ok(uuid)
 }
@@ -45,6 +56,7 @@ pub(crate) fn decode_cbor_uuid(
 pub(crate) fn encode_cbor_uuid<W: minicbor::encode::Write>(
     uuid: uuid::Uuid, e: &mut minicbor::Encoder<W>, (): &mut (),
 ) -> Result<(), minicbor::encode::Error<W::Error>> {
+    e.tag(Tag::new(UUID_CBOR_TAG))?;
     e.bytes(uuid.as_bytes())?;
     Ok(())
 }
