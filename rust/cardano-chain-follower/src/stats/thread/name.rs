8ba010f9cb1f1308539f5a79f350b9aa60529b3e
bkioshn <bkioshn@gmail.com>
2025-01-21 14:24:11 +0700

    fix(cardano-chain-follower): thread stat naming
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/cardano-chain-follower/src/stats/thread/name.rs
+++ rust/cardano-chain-follower/src/stats/thread/name.rs
@@ -1,19 +1,19 @@
 //! Thread names
 
 /// Chain Sync.
-pub(crate) const CHAIN_SYNC: &str = "ChainSync";
+pub(crate) const CHAIN_SYNC: &str = "Async:ChainSync";
 /// Wait for Sync Ready.
-pub(crate) const WAIT_FOR_SYNC_READY: &str = "WaitForSyncReady";
+pub(crate) const WAIT_FOR_SYNC_READY: &str = "Async:WaitForSyncReady";
 /// Live Sync Backfill and Purge.
-pub(crate) const LIVE_SYNC_BACKFILL_AND_PURGE: &str = "LiveSyncBackfillAndPurge";
+pub(crate) const LIVE_SYNC_BACKFILL_AND_PURGE: &str = "Async:LiveSyncBackfillAndPurge";
 /// Mithril Iterator.
 pub(crate) const MITHRIL_ITERATOR: &str = "MithrilIterator";
 /// Background Mithril Snapshot Updater.
-pub(crate) const MITHRIL_SNAPSHOT_UPDATER: &str = "MithrilSnapshotUpdater";
+pub(crate) const MITHRIL_SNAPSHOT_UPDATER: &str = "Async:MithrilSnapshotUpdater";
 /// Mithril compute snapshot.
-pub(crate) const COMPUTE_SNAPSHOT_MSG: &str = "ComputeSnapshotMsg";
+pub(crate) const COMPUTE_SNAPSHOT_MSG: &str = "Async:ComputeSnapshotMsg";
 /// Background Mithril Snapshot Validator.
-pub(crate) const VALIDATE_MITHRIL_SNAPSHOT: &str = "ValidateMithrilSnapshot";
+pub(crate) const VALIDATE_MITHRIL_SNAPSHOT: &str = "Async:ValidateMithrilSnapshot";
 /// Mithril Downloader, Dl and Dedup.
 pub(crate) const MITHRIL_DL_DEDUP: &str = "MithrilDlDedup";
 /// Parallel Download Processor Worker.
