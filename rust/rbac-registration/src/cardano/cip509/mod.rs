26d8a74c30a9940f507e0f3fb4ce5c0dc3f96a38
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-01-27 15:46:52 +0100

    Fix Clippy

--- rust/rbac-registration/src/cardano/cip509/mod.rs
+++ rust/rbac-registration/src/cardano/cip509/mod.rs
@@ -3,6 +3,7 @@
 //! CDDL Reference: <https://github.com/input-output-hk/catalyst-CIPs/blob/x509-envelope-metadata/CIP-XXXX/x509-envelope.cddl>
 
 pub use cip509::Cip509;
+#[allow(clippy::module_name_repetitions)]
 pub use rbac::{C509Cert, Cip509RbacMetadata, SimplePublicKeyType, X509DerCert};
 pub use types::{
     CertKeyHash, KeyLocalRef, LocalRefInt, Payment, PaymentHistory, PointTxnIdx, RoleData,
