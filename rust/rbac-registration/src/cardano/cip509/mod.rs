4fa68e7952c9517025d3d9c1ecf260104ae70dd1
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-01-27 14:13:17 +0100

    Export Cip509RbacMetadata type

--- rust/rbac-registration/src/cardano/cip509/mod.rs
+++ rust/rbac-registration/src/cardano/cip509/mod.rs
@@ -3,7 +3,7 @@
 //! CDDL Reference: <https://github.com/input-output-hk/catalyst-CIPs/blob/x509-envelope-metadata/CIP-XXXX/x509-envelope.cddl>
 
 pub use cip509::Cip509;
-pub use rbac::{C509Cert, SimplePublicKeyType, X509DerCert};
+pub use rbac::{C509Cert, Cip509RbacMetadata, SimplePublicKeyType, X509DerCert};
 pub use types::{
     CertKeyHash, KeyLocalRef, LocalRefInt, Payment, PaymentHistory, PointTxnIdx, RoleData,
     RoleNumber, TxInputHash, ValidationSignature,
