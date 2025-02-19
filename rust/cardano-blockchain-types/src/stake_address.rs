39b2b7283d08e9858463431f1cef945e5341de97
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-02-19 10:20:54 +0100

    Rename TransactionHash to TransactionId

---
+++ rust/cardano-blockchain-types/src/stake_address.rs
@@ -0,0 +1,27 @@
+//! A stake address.
+
+use pallas::ledger::addresses::StakeAddress as PallasStakeAddress;
+
+/// A stake address.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Hash)]
+pub struct StakeAddress(PallasStakeAddress);
+
+impl StakeAddress {
+    pub fn new() -> Self {
+        todo!()
+    }
+}
+
+impl From<PallasStakeAddress> for StakeAddress {
+    fn from(value: PallasStakeAddress) -> Self {
+        Self(value)
+    }
+}
+
+/// This conversion returns a 29 bytes value that includes both header and hash.
+impl From<StakeAddress> for Vec<u8> {
+    fn from(value: StakeAddress) -> Self {
+        // This `to_vec()` call includes both the header and the payload.
+        value.0.to_vec()
+    }
+}
