b9ef5c31b1629dca8a23adc792dc1163125739b2
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-02-27 17:00:04 +0200

    feat(general): Bump `catalyst-ci` (#226)
    * bump cat-ci ver
    
    * fix rust-toolchain
    
    * fix deny.toml
    
    * get rid of the base64ct fix
    
    * try
    
    * add TryFrom<&[u8]> and TryInto<Vec<u8>> impl for CatalystSignedDocument struct
    
    * update test

--- rust/signed_doc/src/lib.rs
+++ rust/signed_doc/src/lib.rs
@@ -258,6 +258,22 @@ impl Encode<()> for CatalystSignedDocument {
     }
 }
 
+impl TryFrom<&[u8]> for CatalystSignedDocument {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(minicbor::decode(value)?)
+    }
+}
+
+impl TryFrom<CatalystSignedDocument> for Vec<u8> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: CatalystSignedDocument) -> Result<Self, Self::Error> {
+        Ok(minicbor::to_vec(value)?)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
@@ -311,8 +327,8 @@ mod tests {
 
         assert!(!doc.problem_report().is_problematic());
 
-        let bytes = minicbor::to_vec(doc).unwrap();
-        let decoded: CatalystSignedDocument = minicbor::decode(bytes.as_slice()).unwrap();
+        let bytes: Vec<u8> = doc.try_into().unwrap();
+        let decoded: CatalystSignedDocument = bytes.as_slice().try_into().unwrap();
 
         assert_eq!(decoded.doc_type().unwrap(), uuid_v4);
         assert_eq!(decoded.doc_id().unwrap(), uuid_v7);
