2008c9c4d4271b4b7d1b4b17e8fa8b57940e0a0b
Steven Johnson <stevenj@users.noreply.github.com>
2025-05-08 23:00:07 +0700

    fix(rust): Update Mithril Crate Dependency (#313)
    * bump to the latest mithril-client version 0.12.0
    
    * wip
    
    * fix
    
    * remove commented out code
    
    * fix
    
    * fix comments
    
    * fix: restore original logic
    
    * fix: Make minimum necessary changes to use new mithril crate version
    
    * fix:
    
    * Update rust/cardano-chain-follower/src/mithril_turbo_downloader.rs
    
    Co-authored-by: Artur Helmanau <artur.helmanau@innowise.com>
    
    * Update rust/cardano-chain-follower/src/mithril_turbo_downloader.rs
    
    Co-authored-by: Artur Helmanau <artur.helmanau@innowise.com>
    
    ---------
    
    Co-authored-by: Mr-Leshiy <leshiy12345678@gmail.com>
    Co-authored-by: Artur Helmanau <artur.helmanau@innowise.com>

--- rust/cardano-chain-follower/src/mithril_snapshot_config.rs
+++ rust/cardano-chain-follower/src/mithril_snapshot_config.rs
@@ -353,7 +353,7 @@ impl MithrilSnapshotConfig {
             .map_err(|e| Error::MithrilClient(self.chain, url.clone(), e))?;
 
         let snapshots = client
-            .snapshot()
+            .cardano_database()
             .list()
             .await
             .map_err(|e| Error::MithrilClient(self.chain, url.clone(), e))?;
