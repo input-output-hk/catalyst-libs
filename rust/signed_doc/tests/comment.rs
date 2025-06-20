4bbc8ae21f8d3153ee07d667e50de644a4fd4e27
bkioshn <35752733+bkioshn@users.noreply.github.com>
2025-06-20 13:28:42 +0700

    fix(rust/signed-doc): Update doc type (#365)
    * fix(signed-doc): log and to value
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): add from doctype to vec uuid
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): add more doc type
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): from doctype for vec string
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): revert
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): revert
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): backward compatible rule
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): remove unused doc type
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): improve static doc types
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    * fix(signed-doc): add more doc-types
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    
    ---------
    
    Signed-off-by: bkioshn <bkioshn@gmail.com>
    Co-authored-by: Alex Pozhylenkov <leshiy12345678@gmail.com>

--- rust/signed_doc/tests/comment.rs
+++ rust/signed_doc/tests/comment.rs
@@ -1,6 +1,8 @@
 //! Integration test for comment document validation part.
 
-use catalyst_signed_doc::{providers::tests::TestCatalystSignedDocumentProvider, *};
+use catalyst_signed_doc::{
+    doc_types::deprecated, providers::tests::TestCatalystSignedDocumentProvider, *,
+};
 use catalyst_types::catalyst_id::role_index::RoleId;
 
 mod common;
@@ -8,16 +10,16 @@ mod common;
 #[tokio::test]
 async fn test_valid_comment_doc() {
     let (proposal_doc, proposal_doc_id, proposal_doc_ver) =
-        common::create_dummy_doc(doc_types::PROPOSAL_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::PROPOSAL_DOCUMENT_UUID_TYPE).unwrap();
     let (template_doc, template_doc_id, template_doc_ver) =
-        common::create_dummy_doc(doc_types::deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
 
     let uuid_v7 = UuidV7::new();
     let (doc, ..) = common::create_dummy_signed_doc(
         serde_json::json!({
             "content-type": ContentType::Json.to_string(),
             "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL_COMMENT_DOC.clone(),
+            "type": doc_types::PROPOSAL_COMMENT.clone(),
             "id": uuid_v7.to_string(),
             "ver": uuid_v7.to_string(),
             "template": {
@@ -46,9 +48,9 @@ async fn test_valid_comment_doc() {
 #[tokio::test]
 async fn test_valid_comment_doc_old_type() {
     let (proposal_doc, proposal_doc_id, proposal_doc_ver) =
-        common::create_dummy_doc(doc_types::PROPOSAL_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::PROPOSAL_DOCUMENT_UUID_TYPE).unwrap();
     let (template_doc, template_doc_id, template_doc_ver) =
-        common::create_dummy_doc(doc_types::deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
 
     let uuid_v7 = UuidV7::new();
     let (doc, ..) = common::create_dummy_signed_doc(
@@ -56,7 +58,7 @@ async fn test_valid_comment_doc_old_type() {
             "content-type": ContentType::Json.to_string(),
             "content-encoding": ContentEncoding::Brotli.to_string(),
             // Using old (single uuid)
-            "type": doc_types::deprecated::COMMENT_DOCUMENT_UUID_TYPE,
+            "type": deprecated::COMMENT_DOCUMENT_UUID_TYPE,
             "id": uuid_v7.to_string(),
             "ver": uuid_v7.to_string(),
             "template": {
@@ -87,9 +89,9 @@ async fn test_valid_comment_doc_with_reply() {
     let empty_json = serde_json::to_vec(&serde_json::json!({})).unwrap();
 
     let (proposal_doc, proposal_doc_id, proposal_doc_ver) =
-        common::create_dummy_doc(doc_types::PROPOSAL_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::PROPOSAL_DOCUMENT_UUID_TYPE).unwrap();
     let (template_doc, template_doc_id, template_doc_ver) =
-        common::create_dummy_doc(doc_types::deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
 
     let comment_doc_id = UuidV7::new();
     let comment_doc_ver = UuidV7::new();
@@ -97,7 +99,7 @@ async fn test_valid_comment_doc_with_reply() {
         .with_json_metadata(serde_json::json!({
             "id": comment_doc_id,
             "ver": comment_doc_ver,
-            "type": doc_types::PROPOSAL_COMMENT_DOC.clone(),
+            "type": doc_types::PROPOSAL_COMMENT.clone(),
             "content-type": ContentType::Json.to_string(),
             "template": { "id": template_doc_id.to_string(), "ver": template_doc_ver.to_string() },
             "ref": {
@@ -115,7 +117,7 @@ async fn test_valid_comment_doc_with_reply() {
         serde_json::json!({
             "content-type": ContentType::Json.to_string(),
             "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL_COMMENT_DOC.clone(),
+            "type": doc_types::PROPOSAL_COMMENT.clone(),
             "id": uuid_v7.to_string(),
             "ver": uuid_v7.to_string(),
             "template": {
@@ -148,16 +150,17 @@ async fn test_valid_comment_doc_with_reply() {
 
 #[tokio::test]
 async fn test_invalid_comment_doc() {
-    let (proposal_doc, ..) = common::create_dummy_doc(doc_types::PROPOSAL_UUID_TYPE).unwrap();
+    let (proposal_doc, ..) =
+        common::create_dummy_doc(deprecated::PROPOSAL_DOCUMENT_UUID_TYPE).unwrap();
     let (template_doc, template_doc_id, template_doc_ver) =
-        common::create_dummy_doc(doc_types::deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
+        common::create_dummy_doc(deprecated::COMMENT_TEMPLATE_UUID_TYPE).unwrap();
 
     let uuid_v7 = UuidV7::new();
     let (doc, ..) = common::create_dummy_signed_doc(
         serde_json::json!({
             "content-type": ContentType::Json.to_string(),
             "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL_COMMENT_DOC.clone(),
+            "type": doc_types::PROPOSAL_COMMENT.clone(),
             "id": uuid_v7.to_string(),
             "ver": uuid_v7.to_string(),
             "template": {
