11d9ed72f082ef794a0c6e9aa841c0025a77a186
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-06-24 08:53:37 +0300

    feat(rust/signed-doc): `*_wrong_role` Catalyst Signed Documents integration tests (#381)
    * more tests
    
    * fix spelling

--- rust/signed_doc/tests/submission.rs
+++ rust/signed_doc/tests/submission.rs
@@ -5,8 +5,16 @@
 use std::sync::LazyLock;
 
 use catalyst_signed_doc::{
-    doc_types::deprecated, providers::tests::TestCatalystSignedDocumentProvider, *,
+    doc_types::deprecated,
+    providers::tests::{TestCatalystSignedDocumentProvider, TestVerifyingKeyProvider},
+    *,
 };
+use catalyst_types::catalyst_id::role_index::RoleId;
+use ed25519_dalek::ed25519::signature::Signer;
+
+use crate::common::create_dummy_key_pair;
+
+mod common;
 
 #[allow(clippy::unwrap_used)]
 static DUMMY_PROPOSAL_DOC: LazyLock<CatalystSignedDocument> = LazyLock::new(|| {
@@ -51,6 +59,10 @@ static DUMMY_BRAND_DOC: LazyLock<CatalystSignedDocument> = LazyLock::new(|| {
 // `parameters` value as `doc`.
 #[tokio::test]
 async fn test_valid_submission_action() {
+    let (sk, pk, kid) = create_dummy_key_pair(RoleId::Proposer).unwrap();
+    let mut key_provider = TestVerifyingKeyProvider::default();
+    key_provider.add_pk(kid.clone(), pk);
+
     // Create a main proposal submission doc, contain all fields mention in the document
     let doc = Builder::new()
         .with_json_metadata(serde_json::json!({
@@ -73,6 +85,8 @@ async fn test_valid_submission_action() {
             "action": "final"
         }))
         .unwrap()
+        .add_signature(|m| sk.sign(&m).to_vec(), kid)
+        .unwrap()
         .build()
         .unwrap();
 
@@ -83,6 +97,51 @@ async fn test_valid_submission_action() {
 
     let is_valid = validator::validate(&doc, &provider).await.unwrap();
     assert!(is_valid, "{:?}", doc.problem_report());
+
+    let is_valid = validator::validate_signatures(&doc, &key_provider)
+        .await
+        .unwrap();
+    assert!(is_valid);
+}
+
+#[tokio::test]
+async fn test_invalid_submission_action_wrong_role() {
+    let (sk, _pk, kid) = create_dummy_key_pair(RoleId::Role0).unwrap();
+
+    // Create a main proposal submission doc, contain all fields mention in the document
+    let doc = Builder::new()
+        .with_json_metadata(serde_json::json!({
+            "content-type": ContentType::Json.to_string(),
+            "content-encoding": ContentEncoding::Brotli.to_string(),
+            "type": doc_types::PROPOSAL_SUBMISSION_ACTION.clone(),
+            "id": UuidV7::new(),
+            "ver": UuidV7::new(),
+            "ref": {
+                "id": DUMMY_PROPOSAL_DOC.doc_id().unwrap(),
+                "ver": DUMMY_PROPOSAL_DOC.doc_ver().unwrap(),
+            },
+            "parameters": {
+                "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
+                "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
+            }
+        }))
+        .unwrap()
+        .with_json_content(&serde_json::json!({
+            "action": "final"
+        }))
+        .unwrap()
+        .add_signature(|m| sk.sign(&m).to_vec(), kid)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let mut provider = TestCatalystSignedDocumentProvider::default();
+
+    provider.add_document(None, &DUMMY_PROPOSAL_DOC).unwrap();
+    provider.add_document(None, &DUMMY_BRAND_DOC).unwrap();
+
+    let is_valid = validator::validate(&doc, &provider).await.unwrap();
+    assert!(!is_valid);
 }
 
 #[tokio::test]
