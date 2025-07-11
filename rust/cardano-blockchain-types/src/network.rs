f2b45fa5173fa9a5a3c07c493885da3d775ae243
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2025-07-11 13:26:48 +0200

    adds local mithirl and node addresses

--- rust/cardano-blockchain-types/src/network.rs
+++ rust/cardano-blockchain-types/src/network.rs
@@ -58,12 +58,12 @@ const DEFAULT_MAINNET_MITHRIL_AGGREGATOR: &str =
 
 // Preprod Defaults
 /// Preprod Default Public Cardano Relay.
-const DEFAULT_PREPROD_RELAY: &str = "preprod-node.play.dev.cardano.org:3001";
+const DEFAULT_PREPROD_RELAY: &str = "127.0.0.1:3001";
 /// Preprod network Mithril Signature genesis vkey.
 const DEFAULT_PREPROD_MITHRIL_GENESIS_KEY: &str = include_str!("data/preprod-genesis.vkey");
 /// Default Mithril Aggregator to use.
 const DEFAULT_PREPROD_MITHRIL_AGGREGATOR: &str =
-    "https://NOT_ESISTENT";
+    "http://localhost:8080/aggregator";
 
 // Preview Defaults
 /// Preview Default Public Cardano Relay.
