3b54f12fd4e14723648696376472d58500c21fe6
Apisit Ritruengroj <38898766+apskhem@users.noreply.github.com>
2025-07-18 18:26:57 +0700

    feat(rust/signed-doc): Add a function to convert back `DocType` to deprecated UUID (#426)
    * initial
    
    * Update rust/signed_doc/src/metadata/doc_type.rs
    
    Co-authored-by: Alex Pozhylenkov <leshiy12345678@gmail.com>
    
    ---------
    
    Co-authored-by: Alex Pozhylenkov <leshiy12345678@gmail.com>

--- rust/signed_doc/src/metadata/doc_type.rs
+++ rust/signed_doc/src/metadata/doc_type.rs
@@ -227,7 +227,7 @@ impl Decode<'_, CompatibilityPolicy> for DocType {
 
 /// Map single UUID doc type to new list of doc types
 /// <https://github.com/input-output-hk/catalyst-libs/blob/main/docs/src/architecture/08_concepts/signed_doc/types.md#document-types>
-fn map_doc_type(uuid: UuidV4) -> DocType {
+pub fn map_doc_type(uuid: UuidV4) -> DocType {
     match uuid {
         id if Uuid::from(id) == deprecated::PROPOSAL_DOCUMENT_UUID_TYPE => PROPOSAL.clone(),
         id if Uuid::from(id) == deprecated::COMMENT_DOCUMENT_UUID_TYPE => PROPOSAL_COMMENT.clone(),
@@ -235,6 +235,19 @@ fn map_doc_type(uuid: UuidV4) -> DocType {
             PROPOSAL_SUBMISSION_ACTION.clone()
         },
         id => DocType(vec![id]),
+    }
+}
+
+/// Maps `DocType` to the deprecated corresponding doc type.
+pub fn to_deprecated_doc_type(doc_type: &DocType) -> Option<UuidV4> {
+    if doc_type == &*PROPOSAL {
+        UuidV4::try_from(deprecated::PROPOSAL_DOCUMENT_UUID_TYPE).ok()
+    } else if doc_type == &*PROPOSAL_COMMENT {
+        UuidV4::try_from(deprecated::COMMENT_DOCUMENT_UUID_TYPE).ok()
+    } else if doc_type == &*PROPOSAL_SUBMISSION_ACTION {
+        UuidV4::try_from(deprecated::PROPOSAL_ACTION_DOCUMENT_UUID_TYPE).ok()
+    } else {
+        doc_type.0.first().cloned()
     }
 }
 
