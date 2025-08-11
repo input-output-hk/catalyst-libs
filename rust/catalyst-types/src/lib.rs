c8dfa685c0e4d8cf8c03c681ca1839b3419ba5a3
bkioshn <bkioshn@gmail.com>
2025-08-11 16:37:34 +0700

    feat: make wasm a feature
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/catalyst-types/src/lib.rs
+++ rust/catalyst-types/src/lib.rs
@@ -3,8 +3,7 @@
 pub mod catalyst_id;
 pub mod cbor_utils;
 pub mod conversion;
-pub mod hash_wrapper;
-pub mod hashes;
+#[cfg(feature = "mmap")]
 pub mod mmap_file;
 pub mod problem_report;
 pub mod uuid;
