0f926c599b20c8f69041476d88aa199c0629e5e1
bkioshn <bkioshn@gmail.com>
2025-08-13 10:54:34 +0700

    fix: remove feat and use target arch
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- rust/catalyst-types/src/lib.rs
+++ rust/catalyst-types/src/lib.rs
@@ -3,7 +3,7 @@
 pub mod catalyst_id;
 pub mod cbor_utils;
 pub mod conversion;
-#[cfg(feature = "mmap")]
+#[cfg(not(target_arch = "wasm32"))]
 pub mod mmap_file;
 pub mod problem_report;
 pub mod uuid;
