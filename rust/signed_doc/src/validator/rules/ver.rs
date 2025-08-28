61ea015925fef28ed7b709e8fa83cdfe5353e8bf
Mr-Leshiy <leshiy12345678@gmail.com>
2025-08-28 11:19:26 +0400

    wip

--- rust/signed_doc/src/validator/rules/ver.rs
+++ rust/signed_doc/src/validator/rules/ver.rs
@@ -1,8 +1,6 @@
 //! Validator for Signed Document Version
 
-use crate::{
-    providers::CatalystSignedDocumentProvider, CatalystSignedDocument, DocLocator, DocumentRef,
-};
+use crate::{providers::CatalystSignedDocumentProvider, CatalystSignedDocument};
 
 /// Signed Document `ver` field validation rule
 pub(crate) struct VerRule;
@@ -43,17 +41,54 @@ impl VerRule {
                 &format!("Document Version {ver} cannot be smaller than Document ID {id}"),
             );
             is_valid = false;
-        }
+        } else if let Some(last_doc) = provider.try_get_last_doc(id).await? {
+            let Ok(last_doc_ver) = last_doc.doc_ver() else {
+                doc.report().missing_field(
+                    "ver",
+                    &format!(
+                        "Missing `ver` field in the latest known document, for the the id {id}"
+                    ),
+                );
+                return Ok(false);
+            };
 
-        if ver != id {
-            let first_submitted_doc = DocumentRef::new(id, id, DocLocator::default());
-            if provider.try_get_doc(&first_submitted_doc).await?.is_none() {
+            if last_doc_ver >= ver {
                 doc.report().functional_validation(
-                    &format!("`ver` and `id` are not equal, ver: {ver}, id: {id}. Document with `id` and `ver` being equal MUST exist"),
-                    "Cannot get a first version document from the provider, document for which `id` and `ver` are equal.",
+                    &format!("New document ver should be greater that the submitted latest known. New document ver: {ver}, latest known ver: {last_doc_ver}"),
+                    &format!("Document's `ver` field should continuously incrising, for the the id {id}"),
                 );
                 is_valid = false;
             }
+
+            let Ok(last_doc_type) = last_doc.doc_type() else {
+                doc.report().missing_field(
+                    "type",
+                    &format!(
+                        "Missing `type` field in the latest known document. Last known document id: {id}, ver: {last_doc_ver}."
+                    ),
+                );
+                return Ok(false);
+            };
+
+            let Ok(doc_type) = doc.doc_type() else {
+                doc.report()
+                    .missing_field("type", &format!("Missing `type` field."));
+                return Ok(false);
+            };
+
+            if last_doc_type != doc_type {
+                doc.report().functional_validation(
+                    &format!("New document type should be the same that the submitted latest known. New document type: {doc_type}, latest known ver: {last_doc_type}"),
+                    &format!("Document's type should be the same for all documents with the same id {id}"),
+                );
+                is_valid = false;
+            }
+        } else if ver != id {
+            doc.report().functional_validation(
+                &format!("`ver` and `id` are not equal, ver: {ver}, id: {id}. Document with `id` and `ver` being equal MUST exist"),
+                "Cannot get a first version document from the provider, document for which `id` and `ver` are equal.",
+            );
+            is_valid = false;
         }
 
         Ok(is_valid)
@@ -70,7 +105,7 @@ mod tests {
     use super::*;
     use crate::{
         builder::tests::Builder, metadata::SupportedField,
-        providers::tests::TestCatalystSignedDocumentProvider, UuidV7,
+        providers::tests::TestCatalystSignedDocumentProvider, UuidV4, UuidV7,
     };
 
     #[test_case(
@@ -86,6 +121,7 @@ mod tests {
     )]
     #[test_case(
         |provider| {
+            let doc_type = UuidV4::new();
             let now = SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
@@ -96,6 +132,7 @@ mod tests {
             let first_doc = Builder::new()
                 .with_metadata_field(SupportedField::Id(id))
                 .with_metadata_field(SupportedField::Ver(id))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
                 .build();
             provider.add_document(None, &first_doc).unwrap();
 
@@ -105,6 +142,7 @@ mod tests {
             Builder::new()
                 .with_metadata_field(SupportedField::Id(id))
                 .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
                 .build()
         }
         => true;
@@ -112,16 +150,18 @@ mod tests {
     )]
     #[test_case(
         |provider| {
+            let doc_type = UuidV4::new();
             let now = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
             let id = Uuid::new_v7(Timestamp::from_unix_time(now + 1, 0, 0, 0))
                 .try_into()
                 .unwrap();
             let first_doc = Builder::new()
                 .with_metadata_field(SupportedField::Id(id))
                 .with_metadata_field(SupportedField::Ver(id))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
                 .build();
             provider.add_document(None, &first_doc).unwrap();
 
@@ -131,13 +171,55 @@ mod tests {
             Builder::new()
                 .with_metadata_field(SupportedField::Id(id))
                 .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
                 .build()
         }
         => false;
         "`ver` less than `id`"
     )]
     #[test_case(
+        |provider| {
+            let doc_type = UuidV4::new();
+            let now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let id = Uuid::new_v7(Timestamp::from_unix_time(now + 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            let doc = Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(id))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
+                .build();
+            provider.add_document(None, &doc).unwrap();
+
+
+            let ver = Uuid::new_v7(Timestamp::from_unix_time(now + 3, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            let doc = Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
+                .build();
+            provider.add_document(None, &doc).unwrap();
+
+            let ver = Uuid::new_v7(Timestamp::from_unix_time(now + 2, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
+                .build()
+        }
+        => false;
+        "`ver` less than `ver` field for of the latest known document"
+    )]
+    #[test_case(
         |_| {
+            let doc_type = UuidV4::new();
             let now = SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
@@ -151,10 +233,95 @@ mod tests {
             Builder::new()
                 .with_metadata_field(SupportedField::Id(id))
                 .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
                 .build()
         }
         => false;
         "missing first version document"
+    )]
+    #[test_case(
+        |provider| {
+            let doc_type = UuidV4::new();
+            let now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let id = Uuid::new_v7(Timestamp::from_unix_time(now - 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            let first_doc = Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(id))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
+                .build();
+            provider.add_document(None, &first_doc).unwrap();
+
+            let ver = Uuid::new_v7(Timestamp::from_unix_time(now + 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(ver))
+                .build()
+        }
+        => false;
+        "missing `type` field"
+    )]
+    #[test_case(
+        |provider| {
+            let doc_type = UuidV4::new();
+            let now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let id = Uuid::new_v7(Timestamp::from_unix_time(now - 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            let first_doc = Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(id))
+                .build();
+            provider.add_document(None, &first_doc).unwrap();
+
+            let ver = Uuid::new_v7(Timestamp::from_unix_time(now + 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(doc_type.into()))
+                .build()
+        }
+        => false;
+        "missing `type` field for the latest known document"
+    )]
+    #[test_case(
+        |provider| {
+            let now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let id = Uuid::new_v7(Timestamp::from_unix_time(now - 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            let first_doc = Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(id))
+                .with_metadata_field(SupportedField::Type(UuidV4::new().into()))
+                .build();
+            provider.add_document(None, &first_doc).unwrap();
+
+            let ver = Uuid::new_v7(Timestamp::from_unix_time(now + 1, 0, 0, 0))
+                .try_into()
+                .unwrap();
+            Builder::new()
+                .with_metadata_field(SupportedField::Id(id))
+                .with_metadata_field(SupportedField::Ver(ver))
+                .with_metadata_field(SupportedField::Type(UuidV4::new().into()))
+                .build()
+        }
+        => false;
+        "diverge `type` field with the latest known document"
     )]
     #[test_case(
         |_| {
