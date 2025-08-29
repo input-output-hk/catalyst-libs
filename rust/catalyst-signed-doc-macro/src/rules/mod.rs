edc55e3483aeca2813017d4e5765d46074b13649
Mr-Leshiy <leshiy12345678@gmail.com>
2025-08-29 11:57:54 +0400

    wip

--- rust/catalyst-signed-doc-macro/src/rules/mod.rs
+++ rust/catalyst-signed-doc-macro/src/rules/mod.rs
@@ -7,12 +7,45 @@ use crate::signed_doc_spec::CatalystSignedDocSpec;
 
 /// `catalyst_signed_documents_rules` macro implementation
 pub(crate) fn catalyst_signed_documents_rules_impl() -> anyhow::Result<TokenStream> {
-    let _spec = CatalystSignedDocSpec::load_signed_doc_spec()?;
+    let spec = CatalystSignedDocSpec::load_signed_doc_spec()?;
+
+    let mut rules_definitions = Vec::new();
+    for (doc_name, _doc_spec) in spec.docs {
+        let const_type_name_ident = doc_name.ident();
+
+        // TODO: impleemnt a proper initialisation for all specific validation rules
+        let rules = quote! {
+            crate::validator::rules::Rules {
+                id: crate::validator::rules::IdRule,
+                ver: crate::validator::rules::VerRule,
+                content_type: crate::validator::rules::ContentTypeRule {
+                    exp: ContentType::Json,
+                },
+                content_encoding: crate::validator::rules::ContentEncodingRule {
+                    exp: ContentEncoding::Brotli,
+                    optional: false,
+                },
+                content: crate::validator::rules::ContentRule::NotSpecified,
+                parameters: crate::validator::rules::ParametersRule::NotSpecified,
+                doc_ref: crate::validator::rules::RefRule::NotSpecified,
+                reply: crate::validator::rules::ReplyRule::NotSpecified,
+                section: crate::validator::rules::SectionRule::NotSpecified,
+                kid: crate::validator::rules::SignatureKidRule {
+                    exp: &[],
+                },
+            }
+        };
+
+        let rule_defintion = quote! {
+            (#const_type_name_ident, #rules),
+        };
+        rules_definitions.push(rule_defintion);
+    }
 
     Ok(quote! {
         /// Returns an iterator with all defined Catalyst Signed Documents validation rules per corresponding document type
-        fn documents_rules() -> impl Iterator<Item = (DocType, Rules)> {
-            [].into_iter()
+        fn documents_rules() -> impl Iterator<Item = (crate::DocType, crate::validator::rules::Rules)> {
+            [ #(#rules_definitions)* ].into_iter()
         }
     })
 }
--- rust/signed_doc/src/validator/mod.rs
+++ rust/signed_doc/src/validator/mod.rs
@@ -17,10 +17,7 @@ use rules::{
 };
 
 use crate::{
-    doc_types::{
-        BRAND_PARAMETERS, CAMPAIGN_PARAMETERS, CATEGORY_PARAMETERS, PROPOSAL, PROPOSAL_COMMENT,
-        PROPOSAL_COMMENT_FORM_TEMPLATE, PROPOSAL_FORM_TEMPLATE, PROPOSAL_SUBMISSION_ACTION,
-    },
+    doc_types::*,
     metadata::DocType,
     providers::{CatalystSignedDocumentProvider, VerifyingKeyProvider},
     signature::{tbs_data, Signature},
