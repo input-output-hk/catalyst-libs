77ae4d2dde63e2df2bfae067babe17083ccb3852
Apisit Ritruengroj <38898766+apskhem@users.noreply.github.com>
2025-09-04 14:22:46 +0700

    feat(rust/signed-doc): Add `NotSpecified` to `ContentTypeRule` (#532)
    * initial
    
    * fix: macro
    
    * fix
    
    * fix: not specified
    
    * fix: not specified
    
    * fix: not specified
    
    * fix: syntax error
    
    * chore: lintfix
    
    * feat: unit test
    
    * chore: doc

--- rust/signed_doc/src/validator/mod.rs
+++ rust/signed_doc/src/validator/mod.rs
@@ -41,7 +41,7 @@ fn proposal_rule() -> Rules {
     Rules {
         id: IdRule,
         ver: VerRule,
-        content_type: ContentTypeRule {
+        content_type: ContentTypeRule::Specified {
             exp: ContentType::Json,
         },
         content_encoding: ContentEncodingRule::Specified {
@@ -79,7 +79,7 @@ fn proposal_comment_rule() -> Rules {
     Rules {
         id: IdRule,
         ver: VerRule,
-        content_type: ContentTypeRule {
+        content_type: ContentTypeRule::Specified {
             exp: ContentType::Json,
         },
         content_encoding: ContentEncodingRule::Specified {
@@ -135,7 +135,7 @@ fn proposal_submission_action_rule() -> Rules {
     Rules {
         id: IdRule,
         ver: VerRule,
-        content_type: ContentTypeRule {
+        content_type: ContentTypeRule::Specified {
             exp: ContentType::Json,
         },
         content_encoding: ContentEncodingRule::Specified {
