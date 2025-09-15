ca3503aed9eacee66e1eefb6b4e729f17527c724
Mr-Leshiy <leshiy12345678@gmail.com>
2025-09-15 14:20:28 +0400

    refactor existing proposal tests using test case crate

--- rust/signed_doc/tests/proposal.rs
+++ rust/signed_doc/tests/proposal.rs
@@ -7,6 +7,7 @@ use std::sync::LazyLock;
 use catalyst_signed_doc::{providers::tests::TestCatalystProvider, *};
 use catalyst_types::catalyst_id::role_index::RoleId;
 use ed25519_dalek::ed25519::signature::Signer;
+use test_case::test_case;
 
 use crate::common::create_dummy_key_pair;
 
@@ -55,159 +56,133 @@ static PROPOSAL_TEMPLATE_DOC: LazyLock<CatalystSignedDocument> = LazyLock::new(|
         .unwrap()
 });
 
-// Given a proposal document `doc`:
-//
-// - Parameters:
-// The `parameters` field in `doc` points to a brand document.
-// The parameter rule defines the link reference as `template`, This mean the document
-// that `ref` field in `doc` points to (in this case = `template_doc`), must have the same
-// `parameters` value as `doc`.
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, pk, kid) = create_dummy_key_pair(RoleId::Proposer).unwrap();
+        provider.add_pk(kid.clone(), pk);
+        // Create a main proposal doc, contain all fields mention in the document (except
+        // 'collaborators' and 'revocations')
+        let doc = Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::Json.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL.clone(),
+                "id": id,
+                "ver": id,
+                "template": {
+                    "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
+                    "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
+                },
+                "parameters": {
+                    "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
+                    "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
+                }
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?    
+            .build()?;
+        Ok(doc)
+    }
+    => true
+    ;
+    "valid document"
+)]
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, pk, kid) = create_dummy_key_pair(RoleId::Role0).unwrap();
+        provider.add_pk(kid.clone(), pk);
+        let doc = Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::Json.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL.clone(),
+                "id": id,
+                "ver": id,
+                "template": {
+                    "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
+                    "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
+                },
+                "parameters": {
+                    "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
+                    "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
+                }
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?    
+            .build()?;
+        Ok(doc)
+    }
+    => false
+    ;
+    "wrong role"
+)]
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, pk, kid) = create_dummy_key_pair(RoleId::Proposer).unwrap();
+        provider.add_pk(kid.clone(), pk);
+        let doc = Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::Json.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                    "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
+                    "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
+                }
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?    
+            .build()?;
+        Ok(doc)
+    }
+    => false
+    ;
+    "missing template"
+)]
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, pk, kid) = create_dummy_key_pair(RoleId::Proposer).unwrap();
+        provider.add_pk(kid.clone(), pk);
+        let doc = Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::Json.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL.clone(),
+                "id": id,
+                "ver": id,
+                "template": {
+                    "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
+                    "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
+                },
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?    
+            .build()?;
+        Ok(doc)
+    }
+    => false
+    ;
+    "missing parameters"
+)]
 #[tokio::test]
-async fn test_valid_proposal_doc() {
-    let (sk, pk, kid) = create_dummy_key_pair(RoleId::Proposer).unwrap();
+async fn test_proposal_doc(
+    doc_gen: impl FnOnce(&mut TestCatalystProvider) -> anyhow::Result<CatalystSignedDocument>
+) -> bool {
     let mut provider = TestCatalystProvider::default();
-    provider.add_pk(kid.clone(), pk);
 
-    // Create a main proposal doc, contain all fields mention in the document (except
-    // collaborations and revocations)
-    let id = UuidV7::new();
-    let doc = Builder::new()
-        .with_json_metadata(serde_json::json!({
-            "content-type": ContentType::Json.to_string(),
-            "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL.clone(),
-            "id": id,
-            "ver": id,
-            "template": {
-                "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
-                "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
-            },
-            "parameters": {
-                "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
-                "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
-            }
-        }))
-        .unwrap()
-        .with_json_content(&serde_json::json!({}))
-        .unwrap()
-        .add_signature(|m| sk.sign(&m).to_vec(), kid)
-        .unwrap()
-        .build()
-        .unwrap();
+
+    let doc = doc_gen(&mut provider).unwrap();
 
     provider.add_document(None, &PROPOSAL_TEMPLATE_DOC).unwrap();
     provider.add_document(None, &DUMMY_BRAND_DOC).unwrap();
 
     let is_valid = validator::validate(&doc, &provider).await.unwrap();
-    assert!(is_valid);
-    assert!(is_valid);
-    assert!(!doc.problem_report().is_problematic());
-}
-
-#[tokio::test]
-async fn test_invalid_proposal_doc_wrong_role() {
-    let (sk, _pk, kid) = create_dummy_key_pair(RoleId::Role0).unwrap();
-
-    // Create a main proposal doc, contain all fields mention in the document (except
-    // collaborations and revocations)
-    let id = UuidV7::new();
-    let doc = Builder::new()
-        .with_json_metadata(serde_json::json!({
-            "content-type": ContentType::Json.to_string(),
-            "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL.clone(),
-            "id": id,
-            "ver": id,
-            "template": {
-                "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
-                "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
-            },
-            "parameters": {
-                "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
-                "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
-            }
-        }))
-        .unwrap()
-        .with_json_content(&serde_json::json!({}))
-        .unwrap()
-        .add_signature(|m| sk.sign(&m).to_vec(), kid)
-        .unwrap()
-        .build()
-        .unwrap();
-
-    let mut provider = TestCatalystProvider::default();
-
-    provider.add_document(None, &PROPOSAL_TEMPLATE_DOC).unwrap();
-    provider.add_document(None, &DUMMY_BRAND_DOC).unwrap();
-
-    let is_valid = validator::validate(&doc, &provider).await.unwrap();
-    assert!(!is_valid);
-}
-
-#[tokio::test]
-async fn test_invalid_proposal_doc_missing_template() {
-    let id = UuidV7::new();
-    let doc = Builder::new()
-        .with_json_metadata(serde_json::json!({
-            "content-type": ContentType::Json.to_string(),
-            "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL.clone(),
-            "id": id,
-            "ver": id,
-            // "template": {
-            //     "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
-            //     "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
-            // },
-            "parameters": {
-                "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
-                "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
-            }
-        }))
-        .unwrap()
-        .with_json_content(&serde_json::json!({}))
-        .unwrap()
-        .build()
-        .unwrap();
-
-    let mut provider = TestCatalystProvider::default();
-
-    provider.add_document(None, &PROPOSAL_TEMPLATE_DOC).unwrap();
-    provider.add_document(None, &DUMMY_BRAND_DOC).unwrap();
-
-    let is_valid = validator::validate(&doc, &provider).await.unwrap();
-    assert!(!is_valid);
-}
-
-#[tokio::test]
-async fn test_invalid_proposal_doc_missing_parameters() {
-    let id = UuidV7::new();
-    let doc = Builder::new()
-        .with_json_metadata(serde_json::json!({
-            "content-type": ContentType::Json.to_string(),
-            "content-encoding": ContentEncoding::Brotli.to_string(),
-            "type": doc_types::PROPOSAL.clone(),
-            "id": id,
-            "ver": id,
-            "template": {
-                "id": PROPOSAL_TEMPLATE_DOC.doc_id().unwrap(),
-                "ver": PROPOSAL_TEMPLATE_DOC.doc_ver().unwrap(),
-            },
-            // "parameters": {
-            //     "id": DUMMY_BRAND_DOC.doc_id().unwrap(),
-            //     "ver": DUMMY_BRAND_DOC.doc_ver().unwrap(),
-            // }
-        }))
-        .unwrap()
-        .with_json_content(&serde_json::json!({}))
-        .unwrap()
-        .build()
-        .unwrap();
-
-    let mut provider = TestCatalystProvider::default();
-
-    provider.add_document(None, &PROPOSAL_TEMPLATE_DOC).unwrap();
-    provider.add_document(None, &DUMMY_BRAND_DOC).unwrap();
-
-    let is_valid = validator::validate(&doc, &provider).await.unwrap();
-    assert!(!is_valid);
+    assert_eq!(is_valid, !doc.problem_report().is_problematic());
+    is_valid
 }
