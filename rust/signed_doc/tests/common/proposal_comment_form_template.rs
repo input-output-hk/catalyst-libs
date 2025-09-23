aaee88d7f0fbb9c9cb6a7199775505827ab6f784
Mr-Leshiy <leshiy12345678@gmail.com>
2025-09-23 15:51:20 +0400

    add proposa comment form template integration tests

--- rust/signed_doc/tests/common/proposal_comment_form_template.rs
+++ rust/signed_doc/tests/common/proposal_comment_form_template.rs
@@ -1,20 +1,28 @@
+use catalyst_signed_doc::providers::tests::TestCatalystProvider;
+use ed25519_dalek::ed25519::signature::Signer;
+
 use super::*;
 
 pub fn proposal_comment_form_template_doc(
-    parameters_doc: &CatalystSignedDocument
+    parameters_doc: &CatalystSignedDocument,
+    provider: &mut TestCatalystProvider,
 ) -> anyhow::Result<CatalystSignedDocument> {
+    let id = UuidV7::new();
+    let (sk, _, kid) = create_dummy_key_pair(RoleId::BrandAdmin)
+        .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
     Builder::new()
         .with_json_metadata(serde_json::json!({
             "content-type": ContentType::SchemaJson.to_string(),
             "content-encoding": ContentEncoding::Brotli.to_string(),
             "type": doc_types::PROPOSAL_COMMENT_FORM_TEMPLATE.clone(),
-            "id": UuidV7::new(),
-            "ver": UuidV7::new(),
+            "id": id,
+            "ver": id,
             "parameters": {
                     "id": parameters_doc.doc_id()?,
                     "ver": parameters_doc.doc_ver()?,
                 }
         }))?
         .with_json_content(&serde_json::json!({}))?
+        .add_signature(|m| sk.sign(&m).to_vec(), kid)?
         .build()
 }
---
+++ rust/signed_doc/tests/proposal_comment_form_template.rs
@@ -0,0 +1,154 @@
+//! Integration test for proposal comment form template document validation part.
+//! <https://input-output-hk.github.io/catalyst-libs/architecture/08_concepts/signed_doc/docs/proposal_comment_form_template>
+
+use catalyst_signed_doc::{providers::tests::TestCatalystProvider, *};
+use catalyst_types::catalyst_id::role_index::RoleId;
+use ed25519_dalek::ed25519::signature::Signer;
+use test_case::test_case;
+
+use crate::common::{
+    brand_parameters_doc, campaign_parameters_doc, category_parameters_doc, create_dummy_key_pair,
+    proposal_comment_form_template_doc,
+};
+
+mod common;
+
+#[test_case(
+    |provider| {
+        let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        proposal_comment_form_template_doc(&parameters, provider)
+    }
+    => true
+    ;
+    "valid document with brand 'parameters'"
+)]
+#[test_case(
+    |provider| {
+        let parameters = campaign_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        proposal_comment_form_template_doc(&parameters, provider)
+    }
+    => true
+    ;
+    "valid document with campaign 'parameters'"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        proposal_comment_form_template_doc(&parameters, provider)
+    }
+    => true
+    ;
+    "valid document with category 'parameters'"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::Role0)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_COMMENT_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "wrong role"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::BrandAdmin)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_COMMENT_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .empty_content()?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "empty content"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::BrandAdmin)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "type": doc_types::PROPOSAL_COMMENT_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => true
+    ;
+    "missing 'content-encoding' (optional)"
+)]
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::Role0)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_COMMENT_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "missing parameters"
+)]
+#[tokio::test]
+#[allow(clippy::unwrap_used)]
+async fn test_proposal_comment_form_template_doc(
+    doc_gen: impl FnOnce(&mut TestCatalystProvider) -> anyhow::Result<CatalystSignedDocument>
+) -> bool {
+    let mut provider = TestCatalystProvider::default();
+
+    let doc = doc_gen(&mut provider).unwrap();
+
+    let is_valid = validator::validate(&doc, &provider).await.unwrap();
+    assert_eq!(is_valid, !doc.problem_report().is_problematic());
+    println!("{:?}", doc.problem_report());
+    is_valid
+}
