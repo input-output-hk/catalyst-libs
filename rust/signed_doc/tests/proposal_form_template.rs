0fb5ab952a0261177d1c4247336766fe541ad98a
Mr-Leshiy <leshiy12345678@gmail.com>
2025-09-23 15:39:23 +0400

    wip

--- rust/signed_doc/tests/proposal_form_template.rs
+++ rust/signed_doc/tests/proposal_form_template.rs
@@ -2,10 +2,12 @@
 //! <https://input-output-hk.github.io/catalyst-libs/architecture/08_concepts/signed_doc/docs/proposal_form_template>
 
 use catalyst_signed_doc::{providers::tests::TestCatalystProvider, *};
+use catalyst_types::catalyst_id::role_index::RoleId;
+use ed25519_dalek::ed25519::signature::Signer;
 use test_case::test_case;
 
 use crate::common::{
-    brand_parameters_doc, campaign_parameters_doc, category_parameters_doc,
+    brand_parameters_doc, campaign_parameters_doc, category_parameters_doc, create_dummy_key_pair,
     proposal_form_template_doc,
 };
 
@@ -38,148 +40,106 @@ mod common;
     ;
     "valid document with category 'parameters'"
 )]
-// #[test_case(
-//     |provider| {
-//         let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let template =
-// proposal_form_template_doc(&parameters).inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let id = UuidV7::new();         let (sk, _, kid) =
-// create_dummy_key_pair(RoleId::Role0).inspect(|(_, pk, kid)| provider.add_pk(kid.
-// clone(), *pk))?;         Builder::new()
-// .with_json_metadata(serde_json::json!({                 "content-type":
-// ContentType::Json.to_string(),                 "content-encoding":
-// ContentEncoding::Brotli.to_string(),                 "type":
-// doc_types::PROPOSAL.clone(),                 "id": id,
-//                 "ver": id,
-//                 "template": {
-//                     "id": template.doc_id()?,
-//                     "ver": template.doc_ver()?,
-//                 },
-//                 "parameters": {
-//                     "id": parameters.doc_id()?,
-//                     "ver": parameters.doc_ver()?,
-//                 }
-//             }))?
-//             .with_json_content(&serde_json::json!({}))?
-//             .add_signature(|m| sk.sign(&m).to_vec(), kid)?
-//             .build()
-//     }
-//     => false
-//     ;
-//     "wrong role"
-// )]
-// #[test_case(
-//     |provider| {
-//         let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let template =
-// proposal_form_template_doc(&parameters).inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let id = UuidV7::new();         let (sk, _, kid) =
-// create_dummy_key_pair(RoleId::Proposer).inspect(|(_, pk, kid)| provider.add_pk(kid.
-// clone(), *pk))?;         Builder::new()
-// .with_json_metadata(serde_json::json!({                 "content-type":
-// ContentType::Json.to_string(),                 "content-encoding":
-// ContentEncoding::Brotli.to_string(),                 "type":
-// doc_types::PROPOSAL.clone(),                 "id": id,
-//                 "ver": id,
-//                 "template": {
-//                     "id": template.doc_id()?,
-//                     "ver": template.doc_ver()?,
-//                 },
-//                 "parameters": {
-//                     "id": parameters.doc_id()?,
-//                     "ver": parameters.doc_ver()?,
-//                 }
-//             }))?
-//             .empty_content()?
-//             .add_signature(|m| sk.sign(&m).to_vec(), kid)?
-//             .build()
-//     }
-//     => false
-//     ;
-//     "empty content"
-// )]
-// #[test_case(
-//     |provider| {
-//         let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let template =
-// proposal_form_template_doc(&parameters).inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let id = UuidV7::new();         let (sk, _, kid) =
-// create_dummy_key_pair(RoleId::Proposer).inspect(|(_, pk, kid)| provider.add_pk(kid.
-// clone(), *pk))?;         Builder::new()
-// .with_json_metadata(serde_json::json!({                 "content-type":
-// ContentType::Json.to_string(),                 "type": doc_types::PROPOSAL.clone(),
-//                 "id": id,
-//                 "ver": id,
-//                 "template": {
-//                     "id": template.doc_id()?,
-//                     "ver": template.doc_ver()?,
-//                 },
-//                 "parameters": {
-//                     "id": parameters.doc_id()?,
-//                     "ver": parameters.doc_ver()?,
-//                 }
-//             }))?
-//             .with_json_content(&serde_json::json!({}))?
-//             .add_signature(|m| sk.sign(&m).to_vec(), kid)?
-//             .build()
-//     }
-//     => true
-//     ;
-//     "missing 'content-encoding' (optional)"
-// )]
-// #[test_case(
-//     |provider| {
-//         let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let id = UuidV7::new();
-//         let (sk, _, kid) = create_dummy_key_pair(RoleId::Proposer).inspect(|(_, pk,
-// kid)| provider.add_pk(kid.clone(), *pk))?;         Builder::new()
-//             .with_json_metadata(serde_json::json!({
-//                 "content-type": ContentType::Json.to_string(),
-//                 "content-encoding": ContentEncoding::Brotli.to_string(),
-//                 "type": doc_types::PROPOSAL.clone(),
-//                 "id": id,
-//                 "ver": id,
-//                 "parameters": {
-//                     "id": parameters.doc_id()?,
-//                     "ver": parameters.doc_ver()?,
-//                 }
-//             }))?
-//             .with_json_content(&serde_json::json!({}))?
-//             .add_signature(|m| sk.sign(&m).to_vec(), kid)?
-//             .build()
-//     }
-//     => false
-//     ;
-//     "missing template"
-// )]
-// #[test_case(
-//     |provider| {
-//         let parameters = brand_parameters_doc().inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let template =
-// proposal_form_template_doc(&parameters).inspect(|v| provider.add_document(None,
-// v).unwrap())?;         let id = UuidV7::new();         let (sk, _, kid) =
-// create_dummy_key_pair(RoleId::Proposer).inspect(|(_, pk, kid)| provider.add_pk(kid.
-// clone(), *pk))?;         Builder::new()
-// .with_json_metadata(serde_json::json!({                 "content-type":
-// ContentType::Json.to_string(),                 "content-encoding":
-// ContentEncoding::Brotli.to_string(),                 "type":
-// doc_types::PROPOSAL.clone(),                 "id": id,
-//                 "ver": id,
-//                 "template": {
-//                     "id": template.doc_id()?,
-//                     "ver": template.doc_ver()?,
-//                 },
-//             }))?
-//             .with_json_content(&serde_json::json!({}))?
-//             .add_signature(|m| sk.sign(&m).to_vec(), kid)?
-//             .build()
-//     }
-//     => false
-//     ;
-//     "missing parameters"
-// )]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::Role0)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "wrong role"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::BrandAdmin)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .empty_content()?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "empty content"
+)]
+#[test_case(
+    |provider| {
+        let parameters = category_parameters_doc().inspect(|v| provider.add_document(None, v).unwrap())?;
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::BrandAdmin)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "type": doc_types::PROPOSAL_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+                "parameters": {
+                        "id": parameters.doc_id()?,
+                        "ver": parameters.doc_ver()?,
+                    },
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => true
+    ;
+    "missing 'content-encoding' (optional)"
+)]
+#[test_case(
+    |provider| {
+        let id = UuidV7::new();
+        let (sk, _, kid) = create_dummy_key_pair(RoleId::Role0)
+            .inspect(|(_, pk, kid)| provider.add_pk(kid.clone(), *pk))?;
+        Builder::new()
+            .with_json_metadata(serde_json::json!({
+                "content-type": ContentType::SchemaJson.to_string(),
+                "content-encoding": ContentEncoding::Brotli.to_string(),
+                "type": doc_types::PROPOSAL_FORM_TEMPLATE.clone(),
+                "id": id,
+                "ver": id,
+            }))?
+            .with_json_content(&serde_json::json!({}))?
+            .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+            .build()
+    }
+    => false
+    ;
+    "missing parameters"
+)]
 #[tokio::test]
-// #[allow(clippy::unwrap_used)]
+#[allow(clippy::unwrap_used)]
 async fn test_proposal_form_template_doc(
     doc_gen: impl FnOnce(&mut TestCatalystProvider) -> anyhow::Result<CatalystSignedDocument>
 ) -> bool {
