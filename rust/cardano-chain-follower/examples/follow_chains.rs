1472a100609ccc2f4dcf0ddcc67da7bd69c38bc1
Mr-Leshiy <leshiy12345678@gmail.com>
2025-10-30 10:31:33 +0700

    add devnet configuration for follow chains

--- rust/cardano-chain-follower/examples/follow_chains.rs
+++ rust/cardano-chain-follower/examples/follow_chains.rs
@@ -31,6 +31,7 @@ use tracing_subscriber::EnvFilter;
 fn process_argument() -> (Vec<Network>, ArgMatches) {
     let matches = Command::new("follow_chains")
         .args(&[
+            arg!(--devnet "Follow Devnet network").action(ArgAction::SetTrue),
             arg!(--preprod "Follow Preprod network").action(ArgAction::SetTrue),
             arg!(--preview "Follow Preview network").action(ArgAction::SetTrue),
             arg!(--mainnet "Follow Mainnet network").action(ArgAction::SetTrue),
@@ -89,6 +90,23 @@ fn process_argument() -> (Vec<Network>, ArgMatches) {
     }
     if matches.get_flag("mainnet") || matches.get_flag("all") {
         networks.push(Network::Mainnet);
+    }
+    if matches.get_flag("devnet") || matches.get_flag("all") {
+        networks.push(Network::Devnet {
+            genesis_key: "5b33322c3235332c3138362c3230312c3137372c31312c3131372c3133352c3138372c3136372c3138312c3138382c32322c35392c3230362c3130352c3233312c3135302c3231352c33302c37382c3231322c37362c31362c3235322c3138302c37322c3133342c3133372c3234372c3136312c36385d".to_string(),
+            magic: 42,
+            network_id: 42,
+            byron_epoch_length: 100_000,
+            byron_slot_length: 1000,
+            byron_known_slot: 0,
+            byron_known_hash: "8f8602837f7c6f8b8867dd1cbc1842cf51a27eaed2c70ef48325d00f8efb320f".to_string(),
+            byron_known_time: 1_564_010_416,
+            shelley_epoch_length: 100,
+            shelley_slot_length: 1,
+            shelley_known_slot: 1_598_400,
+            shelley_known_hash: "02b1c561715da9e540411123a6135ee319b02f60b9a11a603d3305556c04329f".to_string(),
+            shelley_known_time: 1_595_967_616,
+        });
     }
 
     (networks, matches)
