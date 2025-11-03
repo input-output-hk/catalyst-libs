be096fe0bbe94cae41517068c7ad644be05ff2b2
Apisit Ritruengroj <apisit_lon@hotmail.com>
2025-11-03 18:18:06 +0700

    initial

--- rust/signed_doc/src/validator/rules/id/mod.rs
+++ rust/signed_doc/src/validator/rules/id/mod.rs
@@ -3,9 +3,7 @@
 #[cfg(test)]
 mod tests;
 
-use std::time::{Duration, SystemTime};
-
-use anyhow::Context;
+use chrono::{Duration, TimeZone, Utc};
 
 use crate::{providers::CatalystSignedDocumentProvider, CatalystSignedDocument};
 
@@ -16,11 +14,10 @@ pub(crate) struct IdRule;
 impl IdRule {
     /// Validates document `id` field on the timestamps:
     /// 1. If `provider.future_threshold()` not `None`, document `id` cannot be too far in
-    ///    the future (`future_threshold` arg) from `SystemTime::now()` based on the
-    ///    provide threshold
-    /// 2. If `provider.future_threshold()` not `None`, document `id` cannot be too far
-    ///    behind (`past_threshold` arg) from `SystemTime::now()` based on the provide
+    ///    the future (`future_threshold` arg) from `Utc::now()` based on the provided
     ///    threshold
+    /// 2. If `provider.past_threshold()` not `None`, document `id` cannot be too far
+    ///    behind (`past_threshold` arg) from `Utc::now()` based on the provided threshold
     #[allow(clippy::unused_async)]
     pub(crate) async fn check<Provider>(
         &self,
@@ -46,46 +43,46 @@ impl IdRule {
             .ok_or(anyhow::anyhow!("Document `id` field must be a UUIDv7"))?
             .to_unix();
 
-        let Some(id_time) =
-            SystemTime::UNIX_EPOCH.checked_add(Duration::new(id_time_secs, id_time_nanos))
-        else {
-            doc.report().invalid_value(
-                    "id",
-                    &id.to_string(),
-                    "Must a valid duration since `UNIX_EPOCH`",
-                    "Cannot instantiate a valid `SystemTime` value from the provided `id` field timestamp.",
-                );
-            return Ok(false);
-        };
+        let id_time = Utc
+            .timestamp_opt(i64::try_from(id_time_secs).unwrap_or(0), id_time_nanos)
+            .single()
+            .ok_or_else(|| anyhow::anyhow!("Invalid timestamp in document `id` field"))?;
 
-        let now = SystemTime::now();
+        let now = Utc::now();
 
-        if let Ok(id_age) = id_time.duration_since(now) {
-            // `now` is earlier than `id_time`
+        let diff = id_time.signed_duration_since(now);
+
+        if diff.num_nanoseconds().unwrap_or(0) > 0 {
+            // id_time is in the future
             if let Some(future_threshold) = provider.future_threshold() {
-                if id_age > future_threshold {
+                let threshold = Duration::from_std(future_threshold)?;
+                if diff > threshold {
                     doc.report().invalid_value(
                         "id",
                         &id.to_string(),
                         "id < now + future_threshold",
-                        &format!("Document Version timestamp {id} cannot be too far in future (threshold: {future_threshold:?}) from now: {now:?}"),
+                        &format!(
+                            "Document Version timestamp {id} cannot be too far in future (threshold: {threshold:?}) from now: {now:?}"
+                        ),
                     );
                     is_valid = false;
                 }
             }
         } else {
-            // `id_time` is earlier than `now`
-            let id_age = now
-                .duration_since(id_time)
-                .context("BUG! `id_time` must be earlier than `now` at this place")?;
+            // id_time is in the past
+            // make positive duration
+            let id_age = diff.abs();
 
             if let Some(past_threshold) = provider.past_threshold() {
-                if id_age > past_threshold {
+                let threshold = Duration::from_std(past_threshold)?;
+                if id_age > threshold {
                     doc.report().invalid_value(
                         "id",
                         &id.to_string(),
                         "id > now - past_threshold",
-                        &format!("Document Version timestamp {id} cannot be too far behind (threshold: {past_threshold:?}) from now: {now:?}",),
+                        &format!(
+                            "Document Version timestamp {id} cannot be too far behind (threshold: {threshold:?}) from now: {now:?}"
+                        ),
                     );
                     is_valid = false;
                 }
