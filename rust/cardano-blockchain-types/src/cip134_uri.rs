fe42df63dfb3f869920e9152091676175d261182
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-11-13 17:51:44 +0700

    feat(rust/cardano-blockchain-types): `Hash` trait impl for `Cip0134Uri` type (#639)
    * add needed Hash trait impl for `Cip0134Uri` type
    
    * fix fmt

--- rust/cardano-blockchain-types/src/cip134_uri.rs
+++ rust/cardano-blockchain-types/src/cip134_uri.rs
@@ -13,7 +13,7 @@ use pallas_addresses::Address;
 /// See the [proposal] for more details.
 ///
 /// [proposal]: https://github.com/cardano-foundation/CIPs/pull/888
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 #[allow(clippy::module_name_repetitions)]
 pub struct Cip0134Uri {
     /// A URI string.
