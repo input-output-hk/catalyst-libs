e76385ba2b7134621db61893fc783b585cc29d26
Mr-Leshiy <leshiy12345678@gmail.com>
2025-11-17 13:23:13 +0700

    fix clippy

--- rust/rbac-registration/src/registration/cardano/mod.rs
+++ rust/rbac-registration/src/registration/cardano/mod.rs
@@ -792,7 +792,7 @@ mod test {
 
         let report = registration.report().to_owned();
         assert!(chain.update_stateless(&registration).is_none());
-        assert!(report.is_problematic(), "{:?}", report);
+        assert!(report.is_problematic(), "{report:?}");
 
         // Add the second registration.
         let data = test::block_6();
