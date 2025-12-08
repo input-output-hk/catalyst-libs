398a247b6a191a36ea8e017d11f836e428c77589
cong-or <conchubhar.gannon@gmail.com>
2025-12-08 12:42:10 +0000

    chore(hermes-ipfs): Format cleanup and transport config notes
      Format where clauses and add TODO comments documenting disabled
      default transport config and TLS/websocket options.

--- rust/hermes-ipfs/src/lib.rs
+++ rust/hermes-ipfs/src/lib.rs
@@ -134,6 +134,10 @@ impl HermesIpfs {
             .enable_dns()
             .with_default()
             .set_default_listener()
+            // TODO(saibatizoku): Re-Enable default transport config when libp2p Cert bug is fixed
+            // TODO(rafal-ch): TLS is disabled by default, we can enable it by calling
+            // on of the `IpfsBuilder::enable_secure...()` flavors.
+            //.enable_secure_websocket()
             .start()
             .await?;
         Ok(HermesIpfs { node })
