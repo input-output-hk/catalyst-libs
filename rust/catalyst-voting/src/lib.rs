eac8813c6c71510f595a5d1a7c38ae1bb022f001
Mr-Leshiy <leshiy12345678@gmail.com>
2025-12-09 19:38:01 +0700

    fix comments

--- rust/catalyst-voting/src/lib.rs
+++ rust/catalyst-voting/src/lib.rs
@@ -1,4 +1,4 @@
-//! Voting primitives which are used among Catalyst ecosystem.
+//! Cryptographic voting primitives which are used among Catalyst ecosystem.
 
 #![allow(clippy::arithmetic_side_effects)]
 
