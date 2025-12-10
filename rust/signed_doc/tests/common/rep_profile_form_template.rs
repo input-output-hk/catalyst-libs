ede956b08899d395c1438eb30f1b0f58c5273722
Mr-Leshiy <leshiy12345678@gmail.com>
2025-12-10 11:11:12 +0700

    add rep_profile_form_template tests

---
+++ rust/signed_doc/tests/common/rep_profile_form_template.rs
@@ -0,0 +1,28 @@
+use catalyst_signed_doc::providers::tests::TestCatalystProvider;
+use ed25519_dalek::ed25519::signature::Signer;
+
+use super::*;
+
+pub fn rep_profile_form_template_doc(
+    parameters: &CatalystSignedDocument,
+    provider: &mut TestCatalystProvider,
+) -> anyhow::Result<CatalystSignedDocument> {
+    let id = UuidV7::new();
+    let (sk, kid) = create_dummy_key_pair(None);
+    provider.add_sk(kid.clone(), sk.clone());
+
+    let parameters_ref = parameters.doc_ref()?;
+
+    Builder::new()
+        .with_json_metadata(serde_json::json!({
+            "content-type": ContentType::SchemaJson,
+            "content-encoding": ContentEncoding::Brotli,
+            "id": id,
+            "ver": id,
+            "type": doc_types::REP_PROFILE_FORM_TEMPLATE.clone(),
+            "parameters": [parameters_ref]
+        }))?
+        .with_json_content(&serde_json::json!({}))?
+        .add_signature(|m| sk.sign(&m).to_vec(), kid)?
+        .build()
+}
