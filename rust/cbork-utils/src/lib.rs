7d9d06f77041e371b367e62de4654ac7a493cd0a
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-12-22 14:14:16 -0600

    feat(rust/cbork-utils): add CBOR TAG const for BLAKE3 Hash

--- rust/cbork-utils/src/lib.rs
+++ rust/cbork-utils/src/lib.rs
@@ -6,3 +6,7 @@ pub mod decode_helper;
 pub mod deterministic_helper;
 pub mod map;
 pub mod with_cbor_bytes;
+
+/// CBOR tag for BLAKE3 HASH
+/// <https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml>
+pub const BLAKE3_CBOR_TAG: u64 = 32781;
