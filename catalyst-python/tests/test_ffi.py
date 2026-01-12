35fd6b7ff0ac415545017fcd99ee6073761bad83
Mr-Leshiy <leshiy12345678@gmail.com>
2026-01-12 19:03:59 +0700

    fix ruff lints

--- catalyst-python/tests/test_ffi.py
+++ catalyst-python/tests/test_ffi.py
@@ -1,8 +1,8 @@
-# ruff: noqa: S101, ERA001, D100, D103
+# ruff: noqa: S101, D100, D103
 
 from catalyst_python import catalyst_python_ffi
 
 
 def test_ffi_integration() -> None:
-    assert catalyst_python_ffi.ffi_check(True)
-    assert not catalyst_python_ffi.ffi_check(False)
+    assert catalyst_python_ffi.ffi_check(flag=True)
+    assert not catalyst_python_ffi.ffi_check(flag=False)
