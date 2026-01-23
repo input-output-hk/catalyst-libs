deca9c6ca41dc96f9091cd1405a6ed54de40ae6b
Mr-Leshiy <leshiy12345678@gmail.com>
2026-01-23 09:39:27 +0100

    fix tests

--- rust/signed_doc/src/tests_utils/contest_parameters.rs
+++ rust/signed_doc/src/tests_utils/contest_parameters.rs
@@ -5,6 +5,16 @@ use crate::{
     tests_utils::create_dummy_admin_key_pair,
 };
 
+pub fn contest_parameters_default_content() -> serde_json::Value {
+    serde_json::json!({
+        "start": Utc::now(),
+        "end": Utc::now().checked_add_signed(Duration::minutes(5)),
+        "snapshot": Utc::now(),
+        "election_public_key": "0000000000000000000000000000000000000000000000000000000000000000",
+        "choices": ["Yes", "No", "Abstain"]
+    })
+}
+
 pub fn contest_parameters_doc(
     template: &CatalystSignedDocument,
     parameters: &CatalystSignedDocument,
@@ -12,14 +22,7 @@ pub fn contest_parameters_doc(
 ) -> anyhow::Result<CatalystSignedDocument> {
     let (sk, kid) = create_dummy_admin_key_pair();
     provider.add_sk(kid.clone(), sk.clone());
-    let content = serde_json::json!({
-        "start": Utc::now(),
-        "end": Utc::now().checked_add_signed(Duration::minutes(5)),
-        "snapshot": Utc::now(),
-        "election_public_key": "0000000000000000000000000000000000000000000000000000000000000000",
-        "choices": ["Yes", "No"]
-    });
-
+    let content = contest_parameters_default_content();
     builder::contest_parameters_doc(
         &content,
         template,
